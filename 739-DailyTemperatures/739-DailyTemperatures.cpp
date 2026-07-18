662eaf3b6dc8bf1e889300c05c9e717f72ae9328
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:03:33 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/18/2026, 2:03:32 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& a) {
+        int n =a.size();
+        vector<int>ans(n,0);    
+        stack<int>st;
+
+        for(int i = n-1 ; i>= 0 ; i--){
+
+            while(!st.empty() && a[st.top()]<=a[i]) st.pop();
+            if(!st.empty()) ans[i] = st.top()-i ;
+            st.push(i);
+        }
+        return ans ;
+    }
+};
