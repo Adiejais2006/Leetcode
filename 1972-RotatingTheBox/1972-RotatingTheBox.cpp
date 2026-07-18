1960e4190c79f12b7b31754f4efd72751fe88ac0
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:01:26 +0530

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/18/2026, 2:01:26 PM
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+     
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                else if (boxGrid[i][j] == '#') {
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
+                }
+            }
+        }
+
+       
+        vector<vector<char>> ans(n, vector<char>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
