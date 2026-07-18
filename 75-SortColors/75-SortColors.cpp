0340e69709fa08f0bde5de0648294dca2941ba74
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:07:00 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/18/2026, 2:06:59 PM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int j = nums.size() - 1;
+        int k = 0;
+
+        while (k <= j) {
+            if (nums[k] == 0) {
+                swap(nums[k], nums[i]);
+                i++;
+                k++;
+            }
+            else if (nums[k] == 2) {
+                swap(nums[k], nums[j]);
+                j--;
+            }
+            else {
+                k++;
+            }
+        }
+    }
+};
