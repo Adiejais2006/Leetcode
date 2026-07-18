a501b86ae8b455eb0527eda93184dbb70a4ed341
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:05:24 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/18/2026, 2:05:23 PM
+class MyStack {
+private:
+    queue<int> q;
+
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        for (int i = 0; i < sz - 1; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
