2e3ad52085ab0c56d3f5dca6be7f1e0aebfc7036
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:03:55 +0530

    Create 637-AverageOfLevelsInBinaryTree

---
+++ 637-AverageOfLevelsInBinaryTree/637-AverageOfLevelsInBinaryTree.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/18/2026, 2:03:54 PM
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<double> ans;
+        while (!q.empty()) {
+            int size = q.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                auto node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                if (node->right != NULL)
+                    q.push(node->right);
+            }
+            ans.push_back(sum/size);
+        }
+        return ans;
+    }
+};
