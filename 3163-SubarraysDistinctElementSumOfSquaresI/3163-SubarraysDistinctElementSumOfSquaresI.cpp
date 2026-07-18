18132c1fe2e0aef9d541950296c5ee05b0cde791
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 13:59:57 +0530

    Create 3163-SubarraysDistinctElementSumOfSquaresI

---
+++ 3163-SubarraysDistinctElementSumOfSquaresI/3163-SubarraysDistinctElementSumOfSquaresI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/18/2026, 1:59:56 PM
+class Solution {
+public:
+    int sumCounts(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> st;
+            for (int j = i; j < n; j++) {
+                st.insert(nums[j]);
+                int distinct = st.size();
+                ans += distinct * distinct;
+            }
+        }
+
+        return ans;
+    }
+};
