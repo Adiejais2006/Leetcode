0bea04815eed7e051dcb71835e0394364a6d42cc
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:00:51 +0530

    Create 2306-CreateBinaryTreeFromDescriptions

---
+++ 2306-CreateBinaryTreeFromDescriptions/2306-CreateBinaryTreeFromDescriptions.cpp
@@ -0,0 +1,34 @@
+// Last updated: 7/18/2026, 2:00:51 PM
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> childs;
+
+        for (auto &d : descriptions) {
+            int parent = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+
+            if (!mp.count(parent))
+                mp[parent] = new TreeNode(parent);
+
+            if (!mp.count(child))
+                mp[child] = new TreeNode(child);
+
+            if (isLeft)
+                mp[parent]->left = mp[child];
+            else
+                mp[parent]->right = mp[child];
+
+            childs.insert(child);
+        }
+
+        for (auto &[val, node] : mp) {
+            if (!childs.count(val))
+                return node;
+        }
+
+        return nullptr;
+    }
+};
