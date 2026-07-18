217baa7b3b2875e6cb41adf4133e5431e6754056
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:03:58 +0530

    Create 617-MergeTwoBinaryTrees

---
+++ 617-MergeTwoBinaryTrees/617-MergeTwoBinaryTrees.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/18/2026, 2:03:57 PM
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (!root2)
+            return root1;
+        if (!root1)
+            return root2;
+
+        root1->val += root2->val;
+
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+
+        return root1;
+    }
+};
