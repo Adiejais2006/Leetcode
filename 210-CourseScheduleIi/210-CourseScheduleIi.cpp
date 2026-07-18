6dafc47a0a9ca5079229a4744846596152a7ecc7
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:05:35 +0530

    Create 210-CourseScheduleIi

---
+++ 210-CourseScheduleIi/210-CourseScheduleIi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/18/2026, 2:05:34 PM
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& arr) {
+          vector<int> adj[N];
+        vector<int> indegree(N, 0);
+        for (int i = 0; i < arr.size(); i++) {
+            int u = arr[i][1];
+            int v = arr[i][0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+        vector<int> temp;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            temp.push_back(node);
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.push(it);
+            }
+        }
+        if(temp.size()==N)return temp;
+        else 
+        return {};
+    }
+};
