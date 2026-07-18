c962942f45d787314166e160b93f7b79cc18cc0a
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:03:03 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.cpp
@@ -0,0 +1,8 @@
+// Last updated: 7/18/2026, 2:03:02 PM
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
