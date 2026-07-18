184eedd5239dbfff3ee003db49fee24e9b86a608
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:03:25 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/18/2026, 2:03:24 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 ;
+        int n = nums.size();
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if (nums[mid]>target) high =mid-1;
+            else low = mid+1;
+        }
+        return -1;
+    }
+};
