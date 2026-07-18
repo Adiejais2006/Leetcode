91aa408abdd2775b04bbac4955132c1c8793a0c8
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 22:21:00 +0530

    File updated at 7/18/2026, 10:21:00 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,24 +1,21 @@
-// Last updated: 7/18/2026, 2:08:10 PM
-class Solution {
-public:
-    int lengthOfLongestSubstring(string nums) {
-        int n = nums.size();
-        unordered_map<char, int> mpp;
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-
-        while (right < n) {
-            mpp[nums[right]]++;
-
-            while (mpp[nums[right]] > 1) {
-                mpp[nums[left]]--;
-                left++;
-            }
-
-            ans = max(ans, right - left + 1);
-            right++;
-        }
-        return ans;
-    }
-};
+// Last updated: 7/18/2026, 10:21:00 PM
+1class Solution {
+2public:
+3    int lengthOfLongestSubstring(string nums) {
+4        int n = nums.size();
+5        int l = 0;
+6        int r = 0;
+7        int ans = 0;
+8        unordered_map<int, int> mpp;
+9        while (r < n) {
+10            mpp[nums[r]]++;
+11            while (mpp[nums[r]] > 1) {
+12                mpp[nums[l]]--;
+13                l++;
+14            }
+15            ans = max(ans, r - l + 1);
+16            r++;
+17        }
+18        return ans;
+19    }
+20};
