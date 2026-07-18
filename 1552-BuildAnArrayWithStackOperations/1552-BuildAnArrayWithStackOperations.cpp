f1ea82beac584727ce4cbda9077067a338eb83f9
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:01:52 +0530

    Create 1552-BuildAnArrayWithStackOperations

---
+++ 1552-BuildAnArrayWithStackOperations/1552-BuildAnArrayWithStackOperations.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/18/2026, 2:01:51 PM
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans;
+        int j = 0;
+
+        for (int i = 1; i <= n && j < target.size(); i++) {
+            ans.push_back("Push");
+
+            if (i == target[j]) {
+                j++;
+            } else {
+                ans.push_back("Pop");
+            }
+        }
+
+        return ans;
+    }
+};
