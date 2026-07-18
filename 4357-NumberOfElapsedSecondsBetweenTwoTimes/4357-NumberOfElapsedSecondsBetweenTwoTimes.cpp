2912eaf43b06e60389e4da594e6a3f9cdeb97cbe
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 13:58:22 +0530

    Create 4357-NumberOfElapsedSecondsBetweenTwoTimes

---
+++ 4357-NumberOfElapsedSecondsBetweenTwoTimes/4357-NumberOfElapsedSecondsBetweenTwoTimes.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/18/2026, 1:58:22 PM
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int tos(string s){
+        int h=stoi(s.substr(0,2));
+        int m=stoi(s.substr(3,2));
+        int se=stoi(s.substr(6,2));
+        return h*3600+m*60+se;
+    }
+    int secondsBetweenTimes(string startTime, string endTime) {
+        return tos(endTime)-tos(startTime);
+    }
+};
