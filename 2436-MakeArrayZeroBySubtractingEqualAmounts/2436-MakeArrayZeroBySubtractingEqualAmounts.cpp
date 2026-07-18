6c4c9c7ce26f9fcf46a398baba558df3b4c0164c
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:00:44 +0530

    Create 2436-MakeArrayZeroBySubtractingEqualAmounts

---
+++ 2436-MakeArrayZeroBySubtractingEqualAmounts/2436-MakeArrayZeroBySubtractingEqualAmounts.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/18/2026, 2:00:43 PM
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        int count=0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            {
+                int x = nums[i];
+                count++;
+                for(int j=i; j<nums.size(); j++) 
+                {
+                    nums[j] = nums[j]-x;
+                }
+
+            }
+        }
+        return count;
+        
+    }
+};
