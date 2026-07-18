6d0ba415cf979da1217412a982d2b0cf9e293965
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:06:40 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/18/2026, 2:06:40 PM
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+     
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+      
+        if (p == NULL || q == NULL) {
+            return false;
+        }
+      
+        if (p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+       
+        return false;
+    }
+};
