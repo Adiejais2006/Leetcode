f3aa2027272baae6e9d1c4e66e54cb140e50ab6b
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 16:37:46 +0530

    File updated at 7/18/2026, 4:37:45 PM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,34 +1,22 @@
-// Last updated: 7/18/2026, 2:07:24 PM
-class Solution {
-public:
-    int trap(vector<int>& height) {
-         int lmax = 0; 
-        int rmax = 0 ;
-        int total = 0 ;
-        int l = 0 ;
-        int r=height.size()-1;
-        while(l<r){
-            if(height[l]<=height[r]){
-                if(lmax>height[l]){
-                    total+=lmax - height[l];
-                }
-                else{
-                    lmax= height[l];
-                }
-                l++;
-            }
-            else {
-                if(rmax>height[r]){
-                      total+=rmax - height[r];
-                }
-                else{
-                    rmax= height[r];
-                }
-                r--;
-            }
-        }
-
-
-        return total;
-    }
-};
+// Last updated: 7/18/2026, 4:37:45 PM
+1class Solution {
+2public:
+3    int trap(vector<int>& nums) {
+4        int n = nums.size();
+5        int lmax = nums[0];
+6        int rmax = nums[n - 1];
+7        vector<int> lmaxx(n);
+8        vector<int> rmaxx(n);
+9        for (int i = 0; i < n; i++) {
+10            lmax = max(nums[i], lmax);
+11            rmax = max(nums[n - 1 - i], rmax);
+12            lmaxx[i] = max(lmax, nums[i]);
+13            rmaxx[n - i - 1] = max(rmax, nums[n - 1 - i]);
+14        }
+15        int ans = 0;
+16        for (int i = 0; i < n; i++) {
+17            ans += min(lmaxx[i], rmaxx[i]) - nums[i];
+18        }
+19        return ans;
+20    }
+21};
