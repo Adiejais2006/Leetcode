a2ca2620d1e9710cd22d8b0170c735cd324ac5e1
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:00:22 +0530

    Create 2712-FindTheMaximumNumberOfMarkedIndices

---
+++ 2712-FindTheMaximumNumberOfMarkedIndices/2712-FindTheMaximumNumberOfMarkedIndices.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/18/2026, 2:00:21 PM
+class Solution {
+public:
+    int maxNumOfMarkedIndices(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ans = 0, i = 0, j = n/2;
+        while(i < n/2 && j < n) {
+            if(nums[i] * 2 <= nums[j]) ans += 2, i++, j++;
+            else j++;
+        }
+        return ans;
+    }
+};
