cbc47e5b8e0698102eb1ae8a11eb541dc06bb4d1
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 16:58:49 +0530

    File updated at 7/18/2026, 4:58:48 PM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,34 +1,24 @@
-// Last updated: 7/18/2026, 4:49:47 PM
+// Last updated: 7/18/2026, 4:58:48 PM
 1class Solution {
 2public:
-3    int trap(vector<int>& height) {
-4         int lmax = 0; 
-5        int rmax = 0 ;
-6        int total = 0 ;
-7        int l = 0 ;
-8        int r=height.size()-1;
-9        while(l<r){
-10            if(height[l]<=height[r]){
-11                if(lmax>height[l]){
-12                    total+=lmax - height[l];
-13                }
-14                else{
-15                    lmax= height[l];
-16                }
-17                l++;
-18            }
-19            else {
-20                if(rmax>height[r]){
-21                      total+=rmax - height[r];
-22                }
-23                else{
-24                    rmax= height[r];
-25                }
-26                r--;
-27            }
-28        }
-29
-30
-31        return total;
-32    }
-33};
+3    int trap(vector<int>& nums) {
+4        int n = nums.size();
+5        int lmax = 0;
+6        int rmax = 0;
+7        int l = 0;
+8        int r = n - 1;
+9        int ans = 0;
+10        while (l < r) {
+11            if (nums[l] >= nums[r]) {
+12                rmax = max(rmax, nums[r]);
+13                ans += rmax - nums[r];
+14                r--;
+15            } else {
+16                lmax = max(lmax, nums[l]);
+17                ans += lmax - nums[l];
+18                l++;
+19            }
+20        }
+21        return ans;
+22    }
+23};
