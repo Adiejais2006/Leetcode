279d5c0f7d5e0f8f1532476f62ce2ae407eb2930
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:07:10 +0530

    Create 61-RotateList

---
+++ 61-RotateList/61-RotateList.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/18/2026, 2:07:09 PM
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+        ListNode* temp = head;
+        int n = 1;
+        while (temp->next) {
+            temp = temp->next;
+            n++;
+        }
+        temp->next = head;
+        k = k % n;
+        int steps = n - k;
+        ListNode* newTail = head;
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail->next;
+        }
+
+        // Step 5: break circle
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
+    }
+};
