86a5506d3cadf4f334c657a0e9578f6d37b17485
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 23:23:06 +0530

    File updated at 7/18/2026, 11:23:06 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,44 +1,40 @@
-// Last updated: 7/18/2026, 11:03:40 PM
+// Last updated: 7/18/2026, 11:23:06 PM
 1class Solution {
 2public:
 3    string minWindow(string s, string t) {
-4        unordered_map<char, int> need, have;
-5
-6        for (char c : t) need[c]++;
-7
-8        int required = need.size();
-9        int formed = 0;
-10
-11        int l = 0;
-12        int minLen = INT_MAX;
-13        int start = 0;
-14
-15        for (int r = 0; r < s.size(); r++) {
-16            char c = s[r];
-17            have[c]++;
-18
-19            if (need.count(c) && have[c] == need[c])
-20                formed++;
-21
-22            while (formed == required) {
-23                if (r - l + 1 < minLen) {
-24                    minLen = r - l + 1;
-25                    start = l;
-26                }
-27
-28                char leftChar = s[l];
-29                have[leftChar]--;
-30
-31                if (need.count(leftChar) &&
-32                    have[leftChar] < need[leftChar])
-33                    formed--;
-34
-35                l++;
-36            }
-37        }
-38
-39        return minLen == INT_MAX
-40                   ? ""
-41                   : s.substr(start, minLen);
-42    }
-43};
+4        int n = s.size();
+5        int m = t.size();
+6        string ans = "";
+7        int l = 0;
+8        int r = 0;
+9        unordered_map<char, int> tmap, smap;
+10        for (char c : t)
+11            tmap[c]++;
+12        int matched = 0;
+13        int required = tmap.size();
+14        int minlen = INT_MAX;
+15        int start = -1;
+16        int end = -1;
+17        while (r < n) {
+18            char c = s[r];
+19            smap[c]++;
+20            if (tmap.count(c) && smap[c] == tmap[c])
+21                matched++;
+22            while (matched == required) {
+23                if (r - l + 1 < minlen) {
+24                    start = l;
+25                    end = r - l + 1;
+26                    minlen = r - l + 1;
+27                }
+28                smap[s[l]]--;
+29                if (tmap.count(s[l]) && smap[s[l]] < tmap[s[l]])
+30                    matched--;
+31
+32                l++;
+33            }
+34            r++;
+35        }
+36        if(start==-1) return "";
+37        return s.substr(start, end);
+38    }
+39};
