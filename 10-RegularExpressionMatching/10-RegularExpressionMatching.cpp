c772b86a72771e606ef1bae009e4bba29d1a26ba
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:08:02 +0530

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/18/2026, 2:08:01 PM
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[m][n] = true;  
+        for (int i = m; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                bool firstMatch = (i < m && (s[i] == p[j] || p[j] == '.'));
+
+                if (j + 1 < n && p[j + 1] == '*') {
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
