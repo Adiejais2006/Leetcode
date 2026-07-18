79b8d25dc52273937f12d4f0c72bb74fa8fef8da
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:05:04 +0530

    Create 295-FindMedianFromDataStream

---
+++ 295-FindMedianFromDataStream/295-FindMedianFromDataStream.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/18/2026, 2:05:03 PM
+
+
+class MedianFinder {
+private:
+    priority_queue<int> l;
+    priority_queue<int, vector<int>, greater<int>> r;
+
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        l.push(num);
+        r.push(l.top());
+        l.pop();
+        
+        if (l.size() < r.size()) {
+            l.push(r.top());
+            r.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (l.size() > r.size()) {
+            return l.top();
+        }
+        return (l.top() + r.top()) / 2.0;
+    }
+};
