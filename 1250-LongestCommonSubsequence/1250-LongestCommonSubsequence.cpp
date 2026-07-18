44103e7033dae1280179b68a817a231ef0980cc0
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:02:23 +0530

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/18/2026, 2:02:22 PM
+class Solution {
+public:
+    int longestCommonSubsequence(string str1, string str2) {
+           int n = str1.size();
+        int m = str2.size();
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                } else
+                  curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+};
