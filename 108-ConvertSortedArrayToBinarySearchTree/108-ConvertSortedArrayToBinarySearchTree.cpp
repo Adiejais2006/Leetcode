81c445e9ea7c15cf315d43811838b0927261f96c
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:06:31 +0530

    Create 108-ConvertSortedArrayToBinarySearchTree

---
+++ 108-ConvertSortedArrayToBinarySearchTree/108-ConvertSortedArrayToBinarySearchTree.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/18/2026, 2:06:30 PM
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return convert(nums, 0, nums.size() - 1);        
+    }
+
+private:
+    TreeNode* convert(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        
+        int mid = (left + right ) / 2;
+        
+        TreeNode* node = new TreeNode(nums[mid]);
+        
+        node->left = convert(nums, left, mid - 1);
+        node->right = convert(nums, mid + 1, right);
+        
+        return node;
+    }    
+};
