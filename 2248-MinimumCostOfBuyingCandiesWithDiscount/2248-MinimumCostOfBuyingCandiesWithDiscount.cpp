de69d9365eda841727bc1be10cd3034c986a0e33
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 14:00:58 +0530

    Create 2248-MinimumCostOfBuyingCandiesWithDiscount

---
+++ 2248-MinimumCostOfBuyingCandiesWithDiscount/2248-MinimumCostOfBuyingCandiesWithDiscount.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/18/2026, 2:00:57 PM
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        int res = 0;
+        int n = cost.size();
+        for (int i = 0; i < n; ++i) {
+            if (i % 3 != 2) {
+                res += cost[i];
+            }
+        }
+        return res;
+    }
+};
