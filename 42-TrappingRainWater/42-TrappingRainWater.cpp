7bf16b21b1411fbec4890d88b34ad28aba6ea7e9
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-18 16:49:48 +0530

    File updated at 7/18/2026, 4:49:47 PM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,22 +1,34 @@
-// Last updated: 7/18/2026, 4:37:45 PM
+// Last updated: 7/18/2026, 4:49:47 PM
 1class Solution {
 2public:
-3    int trap(vector<int>& nums) {
-4        int n = nums.size();
-5        int lmax = nums[0];
-6        int rmax = nums[n - 1];
-7        vector<int> lmaxx(n);
-8        vector<int> rmaxx(n);
-9        for (int i = 0; i < n; i++) {
-10            lmax = max(nums[i], lmax);
-11            rmax = max(nums[n - 1 - i], rmax);
-12            lmaxx[i] = max(lmax, nums[i]);
-13            rmaxx[n - i - 1] = max(rmax, nums[n - 1 - i]);
-14        }
-15        int ans = 0;
-16        for (int i = 0; i < n; i++) {
-17            ans += min(lmaxx[i], rmaxx[i]) - nums[i];
-18        }
-19        return ans;
-20    }
-21};
+3    int trap(vector<int>& height) {
+4         int lmax = 0; 
+5        int rmax = 0 ;
+6        int total = 0 ;
+7        int l = 0 ;
+8        int r=height.size()-1;
+9        while(l<r){
+10            if(height[l]<=height[r]){
+11                if(lmax>height[l]){
+12                    total+=lmax - height[l];
+13                }
+14                else{
+15                    lmax= height[l];
+16                }
+17                l++;
+18            }
+19            else {
+20                if(rmax>height[r]){
+21                      total+=rmax - height[r];
+22                }
+23                else{
+24                    rmax= height[r];
+25                }
+26                r--;
+27            }
+28        }
+29
+30
+31        return total;
+32    }
+33};
