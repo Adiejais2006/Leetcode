9bb22dc93b9cf5ab2ce101302f15e58a654b4738
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-20 01:19:26 +0530

    File updated at 7/20/2026, 1:19:25 AM

--- 125-ValidPalindrome/125-ValidPalindrome.cpp
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -1,19 +1,19 @@
-// Last updated: 7/20/2026, 1:15:10 AM
+// Last updated: 7/20/2026, 1:19:25 AM
 1class Solution {
 2public:
 3    bool isPalindrome(string s) {
-4        string ans = "";
-5        int n = s.size();
-6        for (auto it : s) {
-7
-8            if (isalnum(it))
-9                ans += tolower(it);
-10        }
-11        int i = 0;
-12        int j = ans.size() - 1;
-13        while (i < j) {
-14            if (ans[i++] != ans[j--])
-15                return false;
+4        int n = s.size();
+5        int i = 0;
+6        int j = n - 1;
+7        while (i < j) {
+8            while (i < j && !isalnum(s[i]))
+9                i++;
+10            while (i < j && !isalnum(s[j]))
+11                j--;
+12            if (tolower(s[i]) != tolower(s[j]))
+13                return false;
+14            i++;
+15            j--;
 16        }
 17        return true;
 18    }
