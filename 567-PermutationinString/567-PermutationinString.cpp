42eecec3436f5e7feb4456f7303ca59162833bc2
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-19 23:13:36 +0530

    File updated at 7/19/2026, 11:13:35 PM

--- 567-PermutationinString/567-PermutationinString.cpp
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -1,20 +1,31 @@
-// Last updated: 7/19/2026, 11:04:21 PM
+// Last updated: 7/19/2026, 11:13:35 PM
 1class Solution {
 2public:
 3    bool checkInclusion(string s1, string s2) {
-4        int m = s1.size(); // jiska dhundna hai
-5        int n = s2.size(); // jisme dhundna hai 
-6        sort(s1.begin() , s1.end());
-7        for(int i = 0 ; i<n-m+1 ; i++){
-8            string ans ="";
-9            for(int j = i ; j < i + m ; j++){
-10                ans+= s2[j];
-11                if(ans.size()==m)
-12                sort(ans.begin() , ans.end());
-13                if(ans==s1) return true;
-14            }
-15        }
-16
-17        return false;
-18    }
-19};
+4        int l = 0;
+5        int r = 0;
+6        int m = s1.size();
+7        int n = s2.size();
+8        if(m>n) return false;
+9        vector<int> to(26);
+10        vector<int> in(26);
+11        for (auto it : s1) {
+12            to[it - 'a']++;
+13        }
+14        for (r; r < m; r++) {
+15            in[s2[r] - 'a']++;
+16        }
+17        if (to == in)
+18            return true;
+19
+20        while (r < n) {
+21            in[s2[r] - 'a']++;
+22            in[s2[l] - 'a']--;
+23            r++;
+24            l++;
+25            if (in == to)
+26                return true;
+27        }
+28        return false;
+29    }
+30};
