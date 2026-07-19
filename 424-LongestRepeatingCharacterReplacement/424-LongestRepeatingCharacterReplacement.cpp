96397bc9db448a98252a2c67ed3320a7cc71f274
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-20 00:41:07 +0530

    File updated at 7/20/2026, 12:41:06 AM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,23 +1,24 @@
-// Last updated: 7/18/2026, 2:04:36 PM
-class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        int l = 0;
-        int r = 0;
-        int ans = 0;
-        int cnt = 0;
-        int n = s.size();
-        unordered_map<int, int> mpp;
-        while (r < n) {
-            mpp[s[r]]++;
-            cnt = max(cnt, mpp[s[r]]);
-            if ((r - l + 1) - cnt > k) {
-                mpp[s[l]]--;
-                l++;
-            }
-            ans = max(ans, r - l + 1);
-            r++;
-        }
-        return ans;
-    }
-};
+// Last updated: 7/20/2026, 12:41:06 AM
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        int l = 0;
+5        int r = 0;
+6        int n = s.size();
+7        int ans = 0;
+8        unordered_map<int, int> mpp;
+9        int mostoccur = 0;
+10        while (r < n) {
+11            mpp[s[r]]++;
+12            mostoccur = max(mostoccur, mpp[s[r]]);
+13            while (r - l + 1 - mostoccur > k) {
+14                mpp[s[l]]--;
+15                l++;
+16            }
+17            ans = max(ans, r - l + 1);
+18            r++;
+19        }
+20
+21        return ans;
+22    }
+23};
