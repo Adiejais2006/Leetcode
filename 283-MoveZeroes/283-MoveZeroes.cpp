30aef8a3d6be98e3be4b0a32564e4cacc723f40f
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-20 01:07:48 +0530

    File updated at 7/20/2026, 1:07:47 AM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,25 +1,13 @@
-// Last updated: 7/18/2026, 2:05:06 PM
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-          int j=-1;
-            for(int i=0;i<nums.size();i++){
-
-                if(nums[i]==0) {j=i;
-                break;
-                }
-
-            }
-                if (j==-1) return;
-            for(int i=j+1;i<nums.size();i++){
-
-                if(nums[i]!=0){
-                    swap(nums[i] ,nums[j]);
-                    j++;
-                }
-
-
-            }
-
-    }
-};
+// Last updated: 7/20/2026, 1:07:47 AM
+1class Solution {
+2public:
+3    void moveZeroes(vector<int>& nums) {
+4        int i = 0;
+5        for (int j = 0; j < nums.size(); j++) {
+6            if (nums[j] != 0) {
+7                swap(nums[j], nums[i]);
+8                i++;
+9            }
+10        }
+11    }
+12};
