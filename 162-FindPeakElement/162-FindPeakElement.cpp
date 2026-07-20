760d5923a6fb87e8af80fa961a1242248b2917fd
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-21 00:07:36 +0530

    File updated at 7/21/2026, 12:07:35 AM

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,25 +1,17 @@
-// Last updated: 7/18/2026, 2:05:50 PM
-class Solution {
-public:
-    int findPeakElement(vector<int>& arr) {
-          int m = arr.size();
-        if(m==1) return 0;
-        if (arr[0] > arr[1]) return 0;
-        if (arr[m - 2] < arr[m - 1]) return m-1;
-        
-        int low = 1 ;
-        int high = m-2;
-        while(low<=high){
-            int mid = (low+high)/2 ;
-            if(arr[mid]>arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
-            // inccreasung curve
-            else if (arr[mid-1] < arr[mid] ) {
-                low = mid+1;
-            }
-            else if (arr[mid-1]> arr[mid]){
-                high = mid-1;
-            }
-        }
-        return -1;
-    }
-};
+// Last updated: 7/21/2026, 12:07:35 AM
+1class Solution {
+2public:
+3    int findPeakElement(vector<int>& nums) {
+4        int n = nums.size();
+5        int low = 0;
+6        int high = n - 1;
+7        while (low <= high) {
+8            int mid = low + (high - low) / 2;
+9            if (mid < n-1 && nums[mid] < nums[mid + 1]) {
+10                low = mid + 1;
+11            } else
+12                high = mid - 1;
+13        }
+14        return low;
+15    }
+16};
