eae44720a7cbf26029d4e1b8787713a5f1c97b6b
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-20 23:55:25 +0530

    File updated at 7/20/2026, 11:55:23 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/20/2026, 11:51:15 PM
+// Last updated: 7/20/2026, 11:55:23 PM
 1class Solution {
 2public:
 3    int findMin(vector<int>& nums) {
@@ -8,16 +8,20 @@
 7        int ans = INT_MAX;
 8        while (low <= high) {
 9            int mid = (low + (high - low) / 2);
-10            // left sorted
-11            ans = min(nums[mid], ans);
-12            if (nums[mid] > nums[high]) {
-13                low = mid + 1;
-14            }
-15            // right sorted
-16            else {
-17                high = mid - 1;
+10            if (nums[low] <= nums[high]) {
+11                ans = min(ans, nums[low]);
+12                break;
+13            }
+14            // left sorted
+15            ans = min(nums[mid], ans);
+16            if (nums[mid] > nums[high]) {
+17                low = mid + 1;
 18            }
-19        }
-20        return ans;
-21    }
-22};
+19            // right sorted
+20            else {
+21                high = mid - 1;
+22            }
+23        }
+24        return ans;
+25    }
+26};
