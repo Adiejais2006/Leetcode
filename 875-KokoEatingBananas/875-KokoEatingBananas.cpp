ed56552f8fac896d0f07e73088f921d4861517ef
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-21 00:40:30 +0530

    File updated at 7/21/2026, 12:40:28 AM

--- 875-KokoEatingBananas/875-KokoEatingBananas.cpp
+++ 875-KokoEatingBananas/875-KokoEatingBananas.cpp
@@ -1,31 +1,27 @@
-// Last updated: 7/21/2026, 12:38:15 AM
-1class Solution { long long helper(int no, vector<int>& nums) {
-2        int n = nums.size();
-3        long long sum = 0;
-4        for (int i = 0; i < n; i++) {
-5            sum = sum + ((nums[i] + no - 1) / no);
-6        }
-7        return sum;
-8    }
-9public:
-10    int minEatingSpeed(vector<int>& nums, int h) {
-11         int n = nums.size();
-12        int low = 1;
-13        int high = INT_MIN;
-14        for (int i = 0; i < n; i++) {
-15            high = max(high, nums[i]);
-16        }
-17        int ans = INT_MAX;
-18        while (low <= high) {
-19            int mid = (low + high) / 2;
-20
-21            if (helper(mid, nums) > h) {
-22                low = mid + 1;
-23            } else {
-24                ans = min(ans, mid);
-25                high = mid - 1;
-26            }
-27        }
-28        return ans;
-29    }
-30};
+// Last updated: 7/21/2026, 12:40:28 AM
+1class Solution {
+2public:
+3    int minEatingSpeed(vector<int>& piles, int h) {
+4        int maxu = INT_MIN;
+5        for (auto it : piles) {
+6            maxu = max(maxu, it);
+7        }
+8        int low = 1;
+9        int high = maxu;
+10        int ans = INT_MAX;
+11        while (low <= high) {
+12            int mid = low + (high - low) / 2;
+13            long long hrs = 0;
+14            ;
+15            for (auto it : piles) {
+16                hrs += (it + mid - 1) / mid;
+17            }
+18            if (hrs > h) {
+19                low = mid + 1;
+20            } else {
+21                high = mid - 1;
+22            }
+23        }
+24        return low;
+25    }
+26};
