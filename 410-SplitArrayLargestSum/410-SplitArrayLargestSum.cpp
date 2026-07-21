87b592d14c7adf5f30580fc7469d749beeae00df
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-21 14:39:58 +0530

    File updated at 7/21/2026, 2:39:57 PM

--- 410-SplitArrayLargestSum/410-SplitArrayLargestSum.cpp
+++ 410-SplitArrayLargestSum/410-SplitArrayLargestSum.cpp
@@ -1,35 +1,37 @@
-// Last updated: 7/18/2026, 2:04:41 PM
-class Solution {int stduent( vector<int> &a , int pages){
-        int pagestudent = 0 ;
-        int count =  1;
-        for(int i = 0 ; i < a.size() ; i++){
-            if((pagestudent + a[i] )<=pages){
-                pagestudent+= a[i];
-            }
-            else {
-                count++;
-                pagestudent=a[i];
-            }
-        }
-        return count;
-    }
-public:
-    int splitArray(vector<int>& a, int k) {
-        int ans =-1;
-        int low = *max_element(a.begin(), a.end());
-        int high = accumulate(a.begin() , a.end() , 0);
-        while(low<=high){
-
-            int mid =(low+high)/2;
-            int val = stduent(a , mid);
-            if (val > k ){
-                low=mid+1;
-            }
-            else if ( val<=k){
-                ans =mid;
-                high=mid-1;
-            }
-        }
-    return ans;
-    }
-};
+// Last updated: 7/21/2026, 2:39:57 PM
+1class Solution {
+2    int helper(vector<int>& nums, int k, int maxsum) {
+3        int sum = 0;
+4        int cnt = 1;
+5        for (auto it : nums) {
+6            sum += it;
+7            if (sum > maxsum) {
+8                sum = it;
+9                cnt++;
+10            }
+11        }
+12        return cnt;
+13    }
+14
+15public:
+16    int splitArray(vector<int>& nums, int k) {
+17        int maxu = INT_MIN;
+18        int sum = 0;
+19        for (auto it : nums) {
+20            maxu = max(maxu, it);
+21            sum += it;
+22        }
+23        int low = maxu;
+24        int high = sum;
+25
+26        while (low <= high) {
+27            int mid = low + (high - low) / 2;
+28
+29            if (helper(nums, k, mid) > k) {
+30                low = mid + 1;
+31            } else
+32                high = mid - 1;
+33        }
+34        return low;
+35    }
+36};
