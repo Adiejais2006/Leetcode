94398fe22e52a39d1be00ce2388be63f4c69b675
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-29 20:26:53 +0530

    File updated at 7/29/2026, 8:26:52 PM

--- 771-JewelsandStones/771-JewelsandStones.cpp
+++ 771-JewelsandStones/771-JewelsandStones.cpp
@@ -1,19 +1,16 @@
-// Last updated: 7/29/2026, 8:24:33 PM
+// Last updated: 7/29/2026, 8:26:52 PM
 1class Solution {
 2public:
 3    int numJewelsInStones(string jewels, string stones) {
-4
-5        int count = 0;
-6
-7        for(int i = 0; i < stones.length(); i++) {
-8
-9            for(int j = 0; j < jewels.size();  j++) {
-10
-11                if(stones[i]==jewels[j]) {
-12                    count++;
-13                }
-14            }
-15        }
-16        return count;
-17    }
-18};
+4        unordered_set<char> mpp;
+5        for (auto it : jewels) {
+6            mpp.insert(it);
+7        }
+8        int cnt = 0;
+9        for (auto it : stones) {
+10            if (mpp.count(it) != 0)
+11                cnt++;
+12        }
+13        return cnt;
+14    }
+15};
