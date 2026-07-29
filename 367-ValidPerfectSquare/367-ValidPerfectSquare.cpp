a254c7a53232f4569e9dcdd29a7cf22f5f1895b9
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-07-29 20:04:53 +0530

    File updated at 7/29/2026, 8:04:52 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
@@ -1,15 +1,22 @@
-// Last updated: 7/29/2026, 7:58:37 PM
+// Last updated: 7/29/2026, 8:04:52 PM
 1class Solution {
 2public:
 3    bool isPerfectSquare(int num) {
-4        for(long long i = 1; i <=num; i++) {
-5            if(i*i==num) {
-6                return 1;
-7            }
-8            else if(i*i>num) {
-9                break;
-10            }
-11        }
-12        return 0;
-13    }
-14};
+4        int l = 2;
+5        int r = num;
+6        if (num == 1)
+7            return 1;
+8        while (l <= r) {
+9            int mid = l + (r - l) / 2;
+10            long long number = (long long)mid * mid;
+11            if ((int)number == num)
+12                return 1;
+13            else if (number > num)
+14                r = mid - 1;
+15            else
+16                l = mid + 1;
+17        }
+18
+19        return 0;
+20    }
+21};
