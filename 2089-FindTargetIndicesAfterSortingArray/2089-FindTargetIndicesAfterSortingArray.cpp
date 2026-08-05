4bf2ccaaff0d1f24fd511440f3bad5feb04b7866
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-08-05 17:04:09 +0530

    File updated at 8/5/2026, 5:04:08 PM

--- 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
+++ 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
@@ -1,25 +1,18 @@
-// Last updated: 8/5/2026, 5:03:13 PM
+// Last updated: 8/5/2026, 5:04:08 PM
 1class Solution {
 2public:
 3    vector<int> targetIndices(vector<int>& nums, int target) {
-4        sort(nums.begin(), nums.end());
-5        int l = 0;
-6        int h = nums.size() - 1;
-7        int ind = -1;
-8        while (l <= h) {
-9            int mid = l + (h - l) / 2;
-10            if (nums[mid] >= target) {
-11                ind = mid;
-12                h = mid - 1;
-13            } else {
-14                l = mid + 1;
-15            }
-16        }
-17        vector<int> ans;
-18        for (ind; ind < nums.size(); ind++) {
-19            if (nums[ind] == target)
-20                ans.push_back(ind);
-21        }
-22        return ans;
-23    }
-24};
+4        int less = 0, equal = 0;
+5
+6        for (int x : nums) {
+7            if (x < target) less++;
+8            else if (x == target) equal++;
+9        }
+10
+11        vector<int> ans;
+12        for (int i = 0; i < equal; i++)
+13            ans.push_back(less + i);
+14
+15        return ans;
+16    }
+17};
