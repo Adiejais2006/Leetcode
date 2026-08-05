ccb89e5c6e099bff23631df4b24ca0a397d466f5
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-08-05 16:06:03 +0530

    File updated at 8/5/2026, 4:06:02 PM

--- 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.cpp
+++ 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.cpp
@@ -1,49 +1,40 @@
-// Last updated: 8/5/2026, 4:04:45 PM
+// Last updated: 8/5/2026, 4:06:02 PM
 1class Solution {
 2public:
 3    vector<int> remainingMethods(int n, int k,
 4                                 vector<vector<int>>& invocations) {
-5        vector<int> ans;
-6        vector<int> adj[n];
-7        for (auto it : invocations) {
-8            int u = it[0];
-9            int v = it[1];
-10            adj[u].push_back(v);
-11        }
-12        set<int> suspicious;
-13        vector<int> vis(n, 0);
-14        queue<int> q;
-15        vis[k] = 1;
-16        q.push(k);
-17        while (!q.empty()) {
-18            auto node = q.front();
-19            q.pop();
-20            suspicious.insert(node);
-21            for (auto it : adj[node]) {
-22                if (!vis[it]) {
-23                    vis[it] = 1;
-24                    q.push(it);
-25                }
-26            }
-27        }
-28        bool notPossible = false;
-29        for (auto it : invocations) {
-30            if (!suspicious.count(it[0]) && suspicious.count(it[1])) {
-31                notPossible = true;
-32                break;
-33            } 
-34        }
-35        if (notPossible) {
-36            vector<int> a;
-37            for (int i = 0; i < n; i++)
-38                a.push_back(i);
-39            return a;
-40        }
-41
-42        for(int i = 0 ; i<n ; i++){
-43            if(!suspicious.count(i)) ans.push_back(i);
-44        }
-45
-46        return ans;
-47    }
-48};
+5        vector<vector<int>> adj(n);
+6        for (auto &it : invocations) {
+7            adj[it[0]].push_back(it[1]);
+8        }
+9        vector<int> vis(n, 0);
+10        queue<int> q;
+11        q.push(k);
+12        vis[k] = 1;
+13        while (!q.empty()) {
+14            int node = q.front();
+15            q.pop();
+16            for (int nxt : adj[node]) {
+17                if (!vis[nxt]) {
+18                    vis[nxt] = 1;
+19                    q.push(nxt);
+20                }
+21            }
+22        }
+23        for (auto &it : invocations) {
+24            if (!vis[it[0]] && vis[it[1]]) {
+25                vector<int> ans;
+26                for (int i = 0; i < n; i++)
+27                    ans.push_back(i);
+28                return ans;
+29            }
+30        }
+31        vector<int> ans;
+32        for (int i = 0; i < n; i++) {
+33            if (!vis[i])
+34                ans.push_back(i);
+35        }
+36
+37        return ans;
+38    }
+39};
