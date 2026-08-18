cfb6ae6194a79a7d401a412a6a4acc4ee6a843f2
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-08-18 15:20:36 +0530

    File updated at 8/18/2026, 3:20:35 PM

--- 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
+++ 92-ReverseLinkedListII/92-ReverseLinkedListII.cpp
@@ -1,52 +1,63 @@
-// Last updated: 8/18/2026, 3:17:22 PM
-1class Solution {
-2    ListNode* reverse(ListNode* head) {
-3        ListNode* prev = NULL;
-4        ListNode* curr = head;
-5        while (curr != NULL) {
-6            ListNode* nextNode = curr->next;
-7            curr->next = prev;
-8            prev = curr;
-9            curr = nextNode;
-10        }
-11        return prev;
-12    }
-13
-14public:
-15    ListNode* reverseBetween(ListNode* head, int left, int right) {
-16        ListNode* temp = head;
-17        ListNode* leftt = NULL;
-18        ListNode* rightt = NULL;
-19        ListNode* leftprev = NULL;
-20        ListNode* rightNext = NULL;
-21        int pos = 1;
-22        while (temp != NULL) {
-23            if (pos == left)
-24                break;
-25            leftprev = temp;
-26            temp = temp->next;
-27            pos++;
-28        }
-29        leftt = temp;
-30        temp = head;
-31        pos = 1;
-32        while (temp != NULL) {
-33            if (pos == right)
-34                break;
-35            temp = temp->next;
-36            pos++;
-37        }
-38        rightt = temp;
-39        rightNext = temp->next;
-40        if (leftprev != NULL)
-41            leftprev->next = NULL;
-42        rightt->next = NULL;
-43        ListNode* revHead = reverse(leftt);
-44        if (leftprev != NULL)
-45            leftprev->next = revHead;
-46        else
-47            head = revHead;
-48        leftt->next = rightNext;
-49        return head;
-50    }
-51};
+// Last updated: 8/18/2026, 3:20:35 PM
+1
+2class Solution {
+3    ListNode* reverse(ListNode* head) {
+4        ListNode* prev = NULL;
+5        ListNode* curr = head;
+6        while (curr != NULL) {
+7            ListNode* nextNode = curr->next;
+8            curr->next = prev;
+9            prev = curr;
+10            curr = nextNode;
+11        }
+12        return prev;
+13    }
+14
+15public:
+16    ListNode* reverseBetween(ListNode* head, int left, int right) {
+17
+18        ListNode* temp = head;
+19        ListNode* leftt = NULL;
+20        ListNode* rightt = NULL;
+21        ListNode* leftprev = NULL;
+22        ListNode* rightNext = NULL;
+23
+24        int pos = 1;
+25
+26        while (temp != NULL) {
+27            if (pos == left)
+28                break;
+29
+30            leftprev = temp;
+31            temp = temp->next;
+32            pos++;
+33        }
+34
+35        leftt = temp;
+36
+37        temp = head;
+38        pos = 1;
+39
+40        while (temp != NULL) {
+41            if (pos == right)
+42                break;
+43
+44            temp = temp->next;
+45            pos++;
+46        }
+47
+48        rightt = temp;
+49        rightNext = temp->next;
+50        if (leftprev != NULL)
+51            leftprev->next = NULL;
+52        rightt->next = NULL;
+53
+54        ListNode* revHead = reverse(leftt);
+55        if (leftprev != NULL)
+56            leftprev->next = revHead;
+57            else head =revHead;
+58        leftt->next = rightNext;
+59
+60        return head;
+61    }
+62};
