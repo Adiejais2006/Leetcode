9a7cdb78a278deddf0ce408136112f43ed90816a
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-08-18 12:46:00 +0530

    File updated at 8/18/2026, 12:46:00 PM

--- 3471-FindtheLargestAlmostMissingInteger/3471-FindtheLargestAlmostMissingInteger.cpp
+++ 3471-FindtheLargestAlmostMissingInteger/3471-FindtheLargestAlmostMissingInteger.cpp
@@ -1,33 +1,28 @@
-// Last updated: 8/18/2026, 12:35:34 PM
+// Last updated: 8/18/2026, 12:46:00 PM
 1class Solution {
 2public:
 3    int largestInteger(vector<int>& nums, int k) {
 4        int n = nums.size();
-5        int ans = -1;
-6        unordered_map<int, int> freq;
-7        unordered_map<int, int> windows;
-8        for (int i = 0; i < k; i++) {
-9            freq[nums[i]]++;
-10        }
-11        for (auto it : freq) {
-12            windows[it.first]++;
-13        }
-14        int l = 0, r = k;
-15        while (r < n) {
-16            freq[nums[l]]--;
-17            if (freq[nums[l]] == 0)
-18                freq.erase(nums[l]);
-19            freq[nums[r]]++;
-20            for (auto it : freq) {
-21                windows[it.first]++;
-22            }
-23            l++;
-24            r++;
-25        }
-26        for (auto it : windows) {
-27            if (it.second == 1)
-28                ans = max(ans, it.first);
-29        }
-30        return ans;
-31    }
-32};
+5        unordered_map<int, int> mpp;
+6        for (int x : nums)
+7            mpp[x]++;
+8        if (k == n)
+9            return *max_element(nums.begin(), nums.end());
+10        if (k == 1) {
+11            int ans = -1;
+12            for (int x : nums) {
+13                if (mpp[x] == 1)
+14                    ans = max(ans, x);
+15            }
+16            return ans;
+17        }
+18        int ans = -1;
+19        if (mpp[nums[0]] == 1)
+20            ans = max(ans, nums[0]);
+21
+22        if (mpp[nums[n - 1]] == 1)
+23            ans = max(ans, nums[n - 1]);
+24
+25        return ans;
+26    }
+27};
