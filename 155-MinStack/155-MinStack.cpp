ec9b7d88ada5c0aca81fd8291b617e1d2b154360
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-01 13:36:26 +0530

    File updated at 9/1/2026, 1:36:26 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,53 +1,54 @@
-// Last updated: 7/18/2026, 2:05:52 PM
-class MinStack {
-    stack<long long> st;
-    long long mini;
-
-public:
-    MinStack() {
-        mini = LLONG_MAX;
-    }
-
-    void push(int value) {
-        long long val = value;
-        if (st.empty()) {
-            mini = val;
-            st.push(val);
-        } else {
-            if (val >= mini) {
-                st.push(val);
-            } else {
-                st.push(2 * val - mini);  // safe now
-                mini = val;
-            }
-        }
-    }
-
-    void pop() {
-        if (st.empty()) return;
-
-        long long x = st.top();
-        st.pop();
-
-        if (x < mini) {
-            mini = 2 * mini - x;
-        }
-    }
-
-    int top() {
-        if (st.empty()) return -1;
-
-        long long x = st.top();
-
-        if (x >= mini) {
-            return (int)x;
-        } else {
-            return (int)mini;
-        }
-    }
-
-    int getMin() {
-        if (st.empty()) return -1;
-        return (int)mini;
-    }
-};
+// Last updated: 9/1/2026, 1:36:26 PM
+1class MinStack {
+2    class Node {
+3    public:
+4        int val;
+5        int minVal;
+6        Node* next;
+7        Node(int value, int minimum) {
+8            val = value;
+9            minVal = minimum;
+10            next = NULL;
+11        }
+12    };
+13    Node* head = NULL;
+14
+15public:
+16    MinStack() {}
+17    void push(int value) {
+18        if (head == NULL) {
+19            head = new Node(value, value);
+20        }
+21        else {
+22            Node* newNode = new Node(value, min(value, head->minVal));
+23            newNode->next = head;
+24            head = newNode;
+25        }
+26    }
+27
+28    void pop() {
+29
+30        if (head == NULL)
+31            return;
+32
+33        Node* temp = head;
+34        head = head->next;
+35        delete temp;
+36    }
+37
+38    int top() {
+39
+40        if (head == NULL)
+41            return -1;
+42
+43        return head->val;
+44    }
+45
+46    int getMin() {
+47
+48        if (head == NULL)
+49            return -1;
+50
+51        return head->minVal;
+52    }
+53};
