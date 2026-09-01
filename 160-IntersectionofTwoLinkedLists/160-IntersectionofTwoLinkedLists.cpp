54a29087eeab4e65fd5c2ae207a135115f39a57a
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-01 22:41:52 +0530

    Create 160-IntersectionofTwoLinkedLists

---
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/1/2026, 10:41:51 PM
+1
+2class Solution {
+3public:
+4    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+5        ListNode* a = headA;
+6        ListNode* b = headB;
+7        while (a != b) {
+8            if (a == NULL)
+9                a = headB;
+10            else
+11                a = a->next;
+12            if (b == NULL)
+13                b = headA;
+14            else
+15                b = b->next;
+16        }
+17        return a;
+18    }
+19};
