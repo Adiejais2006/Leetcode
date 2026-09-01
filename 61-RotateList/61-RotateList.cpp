dc3eeba87e02c953ea40f6d26c0efb671798d959
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-02 01:06:47 +0530

    File updated at 9/2/2026, 1:06:46 AM

--- 61-RotateList/61-RotateList.cpp
+++ 61-RotateList/61-RotateList.cpp
@@ -1,26 +1,27 @@
-// Last updated: 7/18/2026, 2:07:09 PM
-class Solution {
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if (!head || !head->next || k == 0) return head;
-        ListNode* temp = head;
-        int n = 1;
-        while (temp->next) {
-            temp = temp->next;
-            n++;
-        }
-        temp->next = head;
-        k = k % n;
-        int steps = n - k;
-        ListNode* newTail = head;
-        for (int i = 1; i < steps; i++) {
-            newTail = newTail->next;
-        }
-
-        // Step 5: break circle
-        ListNode* newHead = newTail->next;
-        newTail->next = NULL;
-
-        return newHead;
-    }
-};
+// Last updated: 9/2/2026, 1:06:46 AM
+1class Solution {
+2public:
+3    ListNode* rotateRight(ListNode* head, int k) {
+4        if (head == NULL || head->next == NULL)
+5            return head;
+6        ListNode* temp = head;
+7        int n = 0;
+8        while (temp != NULL) {
+9            n++;
+10            temp = temp->next;
+11        }
+12        k = k % n;
+13        if (k == 0)
+14            return head;
+15        temp = head;
+16        for (int i = 1; i < n - k; i++) {
+17            temp = temp->next;
+18        }
+19        ListNode* newHead = temp->next;
+20        temp->next=NULL;
+21        ListNode* temp2 = newHead;
+22        while(temp2->next!=NULL)temp2=temp2->next;
+23        temp2->next = head;
+24        return newHead;
+25    }
+26};
