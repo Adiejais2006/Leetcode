e36e261d46e7f07efbd6962b8860174a87ed0a84
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-02 01:22:26 +0530

    File updated at 9/2/2026, 1:22:25 AM

--- 61-RotateList/61-RotateList.cpp
+++ 61-RotateList/61-RotateList.cpp
@@ -1,27 +1,41 @@
-// Last updated: 9/2/2026, 1:06:46 AM
-1class Solution {
-2public:
-3    ListNode* rotateRight(ListNode* head, int k) {
-4        if (head == NULL || head->next == NULL)
-5            return head;
-6        ListNode* temp = head;
-7        int n = 0;
-8        while (temp != NULL) {
-9            n++;
-10            temp = temp->next;
+// Last updated: 9/2/2026, 1:22:25 AM
+1
+2class Solution {
+3    ListNode* reverse(ListNode* node) {
+4        ListNode* temp = node;
+5        ListNode* prev = NULL;
+6        while (temp != NULL) {
+7            ListNode* nextNode = temp->next;
+8            temp->next = prev;
+9            prev = temp;
+10            temp = nextNode;
 11        }
-12        k = k % n;
-13        if (k == 0)
-14            return head;
-15        temp = head;
-16        for (int i = 1; i < n - k; i++) {
-17            temp = temp->next;
-18        }
-19        ListNode* newHead = temp->next;
-20        temp->next=NULL;
-21        ListNode* temp2 = newHead;
-22        while(temp2->next!=NULL)temp2=temp2->next;
-23        temp2->next = head;
-24        return newHead;
-25    }
-26};
+12        return prev;
+13    }
+14
+15public:
+16    ListNode* rotateRight(ListNode* head, int k) {
+17        if (head == NULL || head->next == NULL)
+18    return head;
+19        ListNode* temp = head;
+20        int n = 0;
+21        while (temp != NULL) {
+22            temp = temp->next;
+23            n++;
+24        }
+25        k = k % n;
+26        if (k == 0)
+27            return head;
+28        ListNode* rHead = reverse(head);
+29        temp = rHead;
+30        for (int i = 1; i < k; i++) {
+31            temp = temp->next;
+32        }
+33        ListNode* nextNode = temp->next;
+34        temp->next = NULL;
+35        ListNode* r1 = reverse(rHead);
+36        ListNode* r2 =  reverse(nextNode);
+37        rHead->next = r2;
+38        return r1;
+39    }
+40};
