bf87965e46a0569208cd301484b9509c5b22533f
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-01 23:06:13 +0530

    File updated at 9/1/2026, 11:06:12 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -1,20 +1,33 @@
-// Last updated: 9/1/2026, 10:59:35 PM
+// Last updated: 9/1/2026, 11:06:12 PM
 1class Solution {
-2public:
-3    bool isPalindrome(ListNode* head) {
-4        vector<int> ans;
-5        ListNode* temp = head;
-6        while(temp!=NULL){
-7            ans.push_back(temp->val);
-8            temp =  temp->next;
-9        }
-10        int i = 0 ;
-11        int j = ans.size()-1;
-12
-13        while(i<j){
-14            if(ans[i++]!=ans[j--]) return false;
-15        }
-16
-17        return true;
-18    }
-19};
+2    ListNode* reverse(ListNode* head) {
+3        ListNode* temp = head;
+4        ListNode* prev = NULL;
+5        while (temp != NULL) {
+6            ListNode* nextNode = temp->next;
+7            temp->next = prev;
+8            prev = temp;
+9            temp = nextNode;
+10        }
+11        return prev;
+12    }
+13
+14public:
+15    bool isPalindrome(ListNode* head) {
+16        ListNode* slow = head;
+17        ListNode* fast = head;
+18        while (fast != NULL && fast->next != NULL) {
+19            slow = slow->next;
+20            fast = fast->next->next;
+21        }
+22        ListNode* reverseHead = reverse(slow);
+23        ListNode* temp = head;
+24        while (temp != NULL && reverseHead != NULL) {
+25            if (temp->val != reverseHead->val)
+26                return false;
+27            temp = temp->next;
+28            reverseHead = reverseHead->next;
+29        }
+30        return true;
+31    }
+32};
