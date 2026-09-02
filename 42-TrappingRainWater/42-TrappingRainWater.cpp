18c8b3fe5b005a8a6c0ce46d1f6954077d334318
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-02 23:14:10 +0530

    File updated at 9/2/2026, 11:14:10 PM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -1,24 +1,24 @@
-// Last updated: 9/2/2026, 11:07:59 PM
+// Last updated: 9/2/2026, 11:14:10 PM
 1class Solution {
 2public:
-3    int trap(vector<int>& nums) {
-4        int n = nums.size();
-5        int lmax = 0;
-6        int rmax = 0;
-7        int l = 0;
-8        int r = n - 1;
-9        int ans = 0;
-10        while (l < r) {
-11            if (nums[l] >= nums[r]) {
-12                rmax = max(rmax, nums[r]);
-13                ans += rmax - nums[r];
-14                r--;
-15            } else {
-16                lmax = max(lmax, nums[l]);
-17                ans += lmax - nums[l];
-18                l++;
-19            }
-20        }
+3    int trap(vector<int>& height) {
+4        int n = height.size();
+5        vector<int> lmax(n);
+6        vector<int> rmax(n);
+7        lmax[0] = height[0];
+8        for (int i = 1; i < n; i++) {
+9            lmax[i] = max(lmax[i - 1], height[i]);
+10        }
+11        rmax[n - 1] = height[n - 1];
+12        for (int i = n - 2; i >= 0; i--) {
+13            rmax[i] = max(rmax[i + 1], height[i]);
+14        }
+15        int ans = 0;
+16        for (int i = 0; i < n; i++) {
+17            int water = min(lmax[i], rmax[i]) - height[i];
+18            ans += water;
+19        }
+20
 21        return ans;
 22    }
 23};
