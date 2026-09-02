761764958a4ecd7b391b2f4b4e2c3337141524e6
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-02 23:25:37 +0530

    File updated at 9/2/2026, 11:25:36 PM

--- 496-NextGreaterElementI/496-NextGreaterElementI.cpp
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -1,27 +1,27 @@
-// Last updated: 7/18/2026, 2:04:18 PM
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> m;
-        stack<int> st;
-        
-        for (int x : nums2) {
-            while (!st.empty() && st.top() < x) {
-                m[st.top()] = x;
-                st.pop();
-            }
-            st.push(x);
-        }
-        
-        vector<int> ans;
-        for (int x : nums1) {
-            if (m.count(x)) {
-                ans.push_back(m[x]);
-            } else {
-                ans.push_back(-1);
-            }
-        }
-        
-        return ans;
-    }
-};
+// Last updated: 9/2/2026, 11:25:36 PM
+1class Solution {
+2public:
+3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+4        stack<int> st;
+5        int n = nums2.size();
+6        vector<int> nge(n);
+7        for (int i = n - 1; i >= 0; i--) {
+8            while (!st.empty() && nums2[st.top()] <= nums2[i])
+9                st.pop();
+10            if (st.empty())
+11                nge[i] = -1;
+12            else
+13                nge[i] = nums2[st.top()];
+14            st.push(i);
+15        }
+16        unordered_map<int, int> mpp;
+17        for (int i = 0; i < n; i++) {
+18            mpp[nums2[i]] = i;
+19        }
+20        vector<int> ans;
+21        for (auto it : nums1) {
+22            ans.push_back(nge[mpp[it]]);
+23        }
+24        return ans;
+25    }
+26};
