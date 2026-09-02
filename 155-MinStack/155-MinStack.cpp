f022c5e6ffe5bd7a0578b88578c180a27d38269c
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-02 18:48:56 +0530

    File updated at 9/2/2026, 6:48:54 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,54 +1,28 @@
-// Last updated: 9/1/2026, 1:36:26 PM
+// Last updated: 9/2/2026, 6:48:54 PM
 1class MinStack {
-2    class Node {
-3    public:
-4        int val;
-5        int minVal;
-6        Node* next;
-7        Node(int value, int minimum) {
-8            val = value;
-9            minVal = minimum;
-10            next = NULL;
-11        }
-12    };
-13    Node* head = NULL;
-14
-15public:
-16    MinStack() {}
-17    void push(int value) {
-18        if (head == NULL) {
-19            head = new Node(value, value);
-20        }
-21        else {
-22            Node* newNode = new Node(value, min(value, head->minVal));
-23            newNode->next = head;
-24            head = newNode;
-25        }
-26    }
+2    stack<int> st;
+3    stack<int> minVal;
+4
+5public:
+6    MinStack() {}
+7
+8    void push(int value) {
+9        st.push(value);
+10        if (minVal.empty()) {
+11            minVal.push(value);
+12        } else {
+13            int curr = minVal.top();
+14            minVal.push(min(value, curr));
+15        }
+16    }
+17
+18    void pop() {
+19        st.pop();
+20        minVal.pop();
+21    }
+22
+23    int top() { return st.top(); }
+24
+25    int getMin() { return minVal.top(); }
+26};
 27
-28    void pop() {
-29
-30        if (head == NULL)
-31            return;
-32
-33        Node* temp = head;
-34        head = head->next;
-35        delete temp;
-36    }
-37
-38    int top() {
-39
-40        if (head == NULL)
-41            return -1;
-42
-43        return head->val;
-44    }
-45
-46    int getMin() {
-47
-48        if (head == NULL)
-49            return -1;
-50
-51        return head->minVal;
-52    }
-53};
