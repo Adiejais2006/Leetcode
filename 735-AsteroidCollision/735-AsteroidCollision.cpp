e4b9097f3f0fad0dd467db6834faef1eeab7fefc
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-03 20:45:26 +0530

    File updated at 9/3/2026, 8:45:24 PM

--- 735-AsteroidCollision/735-AsteroidCollision.cpp
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -1,32 +1,29 @@
-// Last updated: 7/18/2026, 2:03:33 PM
-class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-         stack<int> st;
-        vector<int> ans;
-        int n = asteroids.size();
-        for (int i = 0; i < n; i++) {
-            int curElement = asteroids[i];
-
-            while(!st.empty() && curElement <0 && st.top() >0 && abs(curElement) > st.top()){
-                st.pop();
-            }
-            if (!st.empty() && curElement <0 && st.top() >0 && abs(curElement) < abs(st.top())) {
-                continue;
-            }
-
-            if (!st.empty() && curElement <0 && st.top() >0 && abs(curElement) == abs(st.top())) {
-                st.pop();
-                continue;
-            }
-            st.push(asteroids[i]);
-        }
-
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
-};
+// Last updated: 9/3/2026, 8:45:24 PM
+1class Solution {
+2public:
+3    vector<int> asteroidCollision(vector<int>& asteroids) {
+4        stack<int> st;
+5        for (auto a : asteroids) {
+6            if (a > 0)
+7                st.push(a);
+8            else {
+9                while (!st.empty() && st.top() > 0 && st.top() < abs(a)) {
+10                    st.pop();
+11                }
+12                if (st.empty() || st.top() < 0)
+13                    st.push(a);
+14                if (!st.empty() && st.top() == -a) {
+15                    st.pop();
+16                }
+17            }
+18        }
+19        int size = st.size();
+20        vector<int> ans(size);
+21        int i = size-1;
+22        while (!st.empty()) {
+23            ans[i--] = st.top();
+24            st.pop();
+25        }
+26        return ans;
+27    }
+28};
