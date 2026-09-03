b767be15b23135270f042a1761cd9b04cd197f03
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-03 15:58:26 +0530

    File updated at 9/3/2026, 3:58:25 PM

--- 402-RemoveKDigits/402-RemoveKDigits.cpp
+++ 402-RemoveKDigits/402-RemoveKDigits.cpp
@@ -1,36 +1,28 @@
-// Last updated: 9/3/2026, 3:56:46 PM
+// Last updated: 9/3/2026, 3:58:25 PM
 1class Solution {
 2public:
 3    string removeKdigits(string num, int k) {
-4        stack<int> st;
-5        for (auto it : num) {
-6            while (!st.empty() && k > 0 && st.top() > it) {
+4        stack<char> st;
+5        for (char c : num) {
+6            while (!st.empty() && k > 0 && st.top() > c) {
 7                st.pop();
 8                k--;
 9            }
-10            st.push(it);
+10            st.push(c);
 11        }
-12        if (st.empty())
-13            return "0";
-14        while(!st.empty() && k>0){st.pop();k--;}
-15        string ans = "";
-16        while (!st.empty()) {
-17            ans += st.top();
-18            st.pop();
-19        }
-20        reverse(ans.begin(), ans.end());
-21        int nonzero = 0;
-22        for (auto it : ans) {
-23            if (it != '0')
-24                nonzero++;
-25        }
-26
-27        if (nonzero == 0)
-28            return "0";
-29        int i = 0;
-30        while (ans[i] == '0') {
-31            i++;
-32        }
-33        return ans.substr(i);
-34    }
-35};
+12        while (!st.empty() && k > 0) {
+13            st.pop();
+14            k--;
+15        }
+16        string ans;
+17        while (!st.empty()) {
+18            ans += st.top();
+19            st.pop();
+20        }
+21        reverse(ans.begin(), ans.end());
+22        int i = 0;
+23        while (i < ans.size() && ans[i] == '0')
+24            i++;
+25        return i == ans.size() ? "0" : ans.substr(i);
+26    }
+27};
