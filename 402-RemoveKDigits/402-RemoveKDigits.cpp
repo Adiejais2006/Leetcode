8da0f306f04c7539c50751563c6dba66062f42a6
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-03 15:58:47 +0530

    File updated at 9/3/2026, 3:58:46 PM

--- 402-RemoveKDigits/402-RemoveKDigits.cpp
+++ 402-RemoveKDigits/402-RemoveKDigits.cpp
@@ -1,28 +1,26 @@
-// Last updated: 9/3/2026, 3:58:25 PM
+// Last updated: 9/3/2026, 3:58:46 PM
 1class Solution {
 2public:
 3    string removeKdigits(string num, int k) {
-4        stack<char> st;
-5        for (char c : num) {
-6            while (!st.empty() && k > 0 && st.top() > c) {
-7                st.pop();
-8                k--;
-9            }
-10            st.push(c);
-11        }
-12        while (!st.empty() && k > 0) {
-13            st.pop();
-14            k--;
-15        }
-16        string ans;
-17        while (!st.empty()) {
-18            ans += st.top();
-19            st.pop();
-20        }
-21        reverse(ans.begin(), ans.end());
-22        int i = 0;
-23        while (i < ans.size() && ans[i] == '0')
-24            i++;
-25        return i == ans.size() ? "0" : ans.substr(i);
-26    }
-27};
+4        string st;
+5
+6        for (char c : num) {
+7            while (!st.empty() && k > 0 && st.back() > c) {
+8                st.pop_back();
+9                k--;
+10            }
+11            st.push_back(c);
+12        }
+13
+14        while (k > 0) {
+15            st.pop_back();
+16            k--;
+17        }
+18
+19        int i = 0;
+20        while (i < st.size() && st[i] == '0')
+21            i++;
+22
+23        return i == st.size() ? "0" : st.substr(i);
+24    }
+25};
