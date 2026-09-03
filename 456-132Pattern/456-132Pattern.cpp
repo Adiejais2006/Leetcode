f4327946e0216309c3cf780ec9bcbce7c70bab7a
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-03 18:48:28 +0530

    File updated at 9/3/2026, 6:48:27 PM

--- 456-132Pattern/456-132Pattern.cpp
+++ 456-132Pattern/456-132Pattern.cpp
@@ -1,18 +1,19 @@
-// Last updated: 7/18/2026, 2:04:28 PM
-class Solution {
-public:
-    bool find132pattern(vector<int>& nums) {
-        stack<int> s;
-        int third = INT_MIN;
-
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] < third) return true;
-            while (!s.empty() && s.top() < nums[i]) {
-                third = s.top();
-                s.pop();
-            }
-            s.push(nums[i]);
-        }
-        return false;
-    }
-};
+// Last updated: 9/3/2026, 6:48:27 PM
+1class Solution {
+2public:
+3    bool find132pattern(vector<int>& nums) {
+4        stack<int> st;
+5        int second = INT_MIN;
+6        int n = nums.size();
+7        for (int i = n - 1; i >= 0; i--) {
+8            while (!st.empty() && st.top() < nums[i]) {
+9                second = st.top();
+10                st.pop();
+11            }
+12            if (second != INT_MIN && nums[i] < second)
+13                return true;
+14            st.push(nums[i]);
+15        }
+16        return false;
+17    }
+18};
