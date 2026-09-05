2f965e1b8feb799a70062e6d159c5269550e77ff
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-05 18:17:42 +0530

    File updated at 9/5/2026, 6:17:42 PM

--- 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
+++ 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
@@ -1,31 +1,28 @@
-// Last updated: 9/5/2026, 5:47:05 PM
+// Last updated: 9/5/2026, 6:17:42 PM
 1class Solution {
 2public:
 3    int longestSubarray(vector<int>& nums, int limit) {
-4        deque<int> maxdq;
-5        deque<int> mindq;
+4        int l = 0;
+5        int r = 0;
 6        int n = nums.size();
-7        int l = 0;
-8        int ans = 0;
-9        for (int r = 0; r < n; r++) {
-10            while (!maxdq.empty() && nums[maxdq.back()] <= nums[r]) {
-11                maxdq.pop_back();
-12            }
-13            maxdq.push_back(r);
-14            while (!mindq.empty() && nums[mindq.back()] >= nums[r]) {
-15                mindq.pop_back();
-16            }
-17            mindq.push_back(r);
-18            while (nums[maxdq.front()] - nums[mindq.front()] > limit) {
-19                l++;
-20                while (!maxdq.empty() && maxdq.front() < l)
-21                    maxdq.pop_front();
-22
-23                while (!mindq.empty() && mindq.front() < l)
-24                    mindq.pop_front();
-25            }
-26            ans = max(ans, r - l + 1);
-27        }
-28        return ans;
-29    }
-30};
+7        int ans = 0;
+8        priority_queue<pair<int, int>> maxheap;
+9        priority_queue<pair<int, int>, vector<pair<int, int>>,
+10                       greater<pair<int, int>>>
+11            minheap;
+12        while (r < n) {
+13            maxheap.push({nums[r], r});
+14            minheap.push({nums[r], r});
+15            while (maxheap.top().first - minheap.top().first > limit) {
+16                l = min(maxheap.top().second, minheap.top().second) + 1 ;
+17                while (maxheap.top().second < l)
+18                    maxheap.pop();
+19                while (minheap.top().second < l)
+20                    minheap.pop();
+21            }
+22            ans = max(ans, r - l + 1);
+23            r++;
+24        }
+25        return ans;
+26    }
+27};
