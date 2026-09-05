20c19a872ff53e4f4a7705d0d2c1c81df275106a
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 01:15:44 +0530

    File updated at 9/6/2026, 1:15:44 AM

--- 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
+++ 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.cpp
@@ -1,28 +1,30 @@
-// Last updated: 9/6/2026, 12:54:42 AM
+// Last updated: 9/6/2026, 1:15:44 AM
 1class Solution {
 2public:
 3    int longestSubarray(vector<int>& nums, int limit) {
-4        int l = 0;
-5        int r = 0;
-6        int n = nums.size();
-7        priority_queue<pair<int, int>, vector<pair<int, int>>,
-8                       greater<pair<int, int>>>
-9            minheap;
-10        priority_queue<pair<int, int>> maxheap;
-11        int ans = 0;
-12        while (r < n) {
-13            maxheap.push({nums[r], r});
-14            minheap.push({nums[r], r});
-15            while (maxheap.top().first - minheap.top().first > limit) {
-16                l++;
-17                while (!maxheap.empty() && maxheap.top().second < l)
-18                    maxheap.pop();
-19                while (!minheap.empty() && minheap.top().second < l)
-20                    minheap.pop();
-21            }
-22            ans = max(ans, r - l + 1);
-23            r++;
-24        }
-25        return ans;
-26    }
-27};
+4        int n = nums.size();
+5        int l = 0;
+6        int r = 0;
+7        deque<int> maxd;
+8        deque<int> mind;
+9        int ans = 0;
+10        while (r < n) {
+11            while (!maxd.empty() && nums[maxd.back()] <= nums[r])
+12                maxd.pop_back();
+13            maxd.push_back(r);
+14            while (!mind.empty() && nums[mind.back()] >= nums[r])
+15                mind.pop_back();
+16            mind.push_back(r);
+17            while (nums[maxd.front()] - nums[mind.front()] > limit) {
+18                l++;
+19                while (!maxd.empty() && maxd.front() < l)
+20                    maxd.pop_front();
+21                while (!mind.empty() && mind.front() < l)
+22                    mind.pop_front();
+23            }
+24            ans = max(r - l + 1, ans);
+25            r++;
+26        }
+27        return ans;
+28    }
+29};
