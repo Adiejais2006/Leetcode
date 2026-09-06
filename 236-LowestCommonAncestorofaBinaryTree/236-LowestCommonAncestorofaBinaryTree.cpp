f1eacfd5bed3eb7419f8c0abb601f0a349feefb7
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 16:54:18 +0530

    Create 236-LowestCommonAncestorofaBinaryTree

---
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 4:54:17 PM
+1class Solution {
+2public:
+3    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+4        if (root == NULL)
+5            return root;
+6        if (root == p || root == q)
+7            return root;
+8        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+9        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+10        if (left != NULL && right != NULL)
+11            return root;
+12        else {
+13            return (left != NULL) ? left : right;
+14        }
+15    }
+16};
