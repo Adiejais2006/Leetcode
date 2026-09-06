17d0834adf4e52622e7b2f0a31c07fd55e4868f4
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 16:12:55 +0530

    File updated at 9/6/2026, 4:12:54 PM

--- 112-PathSum/112-PathSum.cpp
+++ 112-PathSum/112-PathSum.cpp
@@ -1,18 +1,26 @@
-// Last updated: 7/18/2026, 2:06:26 PM
-class Solution {
-    bool leaf(TreeNode* root) {
-        return root->left == NULL && root->right == NULL;
-    }
-    bool dfs(TreeNode* root, int target) {
-        if (root == NULL)
-            return false;
-        if (leaf(root))
-            return target == root->val;
-
-        return dfs(root->left, target - root->val) ||
-               dfs(root->right, target - root->val);
-    }
-
-public:
-    bool hasPathSum(TreeNode* root, int target) { return dfs(root, target); }
-};
+// Last updated: 9/6/2026, 4:12:54 PM
+1class Solution {
+2    bool isLeaf(TreeNode* root) {
+3        if (root->left == NULL && root->right == NULL)
+4            return true;
+5        return false;
+6    }
+7    bool func(TreeNode* root, int sum, int target) {
+8        if (root == NULL)
+9            return false;
+10        sum += root->val;
+11        if (isLeaf(root)) {
+12            if (sum == target)
+13                return true;
+14            return false;
+15        }
+16        bool left = func(root->left, sum, target);
+17        bool right = func(root->right, sum, target);
+18        return left || right;
+19    }
+20
+21public:
+22    bool hasPathSum(TreeNode* root, int targetSum) {
+23        return func(root, 0, targetSum);
+24    }
+25};
