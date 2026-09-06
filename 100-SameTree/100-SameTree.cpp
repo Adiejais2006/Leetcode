a89d0632ff5e3ceeb7c3f975142019b2d881e039
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 14:36:14 +0530

    File updated at 9/6/2026, 2:36:13 PM

--- 100-SameTree/100-SameTree.cpp
+++ 100-SameTree/100-SameTree.cpp
@@ -1,20 +1,15 @@
-// Last updated: 7/18/2026, 2:06:40 PM
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-     
-        if (p == NULL && q == NULL) {
-            return true;
-        }
-      
-        if (p == NULL || q == NULL) {
-            return false;
-        }
-      
-        if (p->val == q->val) {
-            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        }
-       
-        return false;
-    }
-};
+// Last updated: 9/6/2026, 2:36:13 PM
+1
+2class Solution {
+3    bool func(TreeNode* p, TreeNode* q){
+4        if(p==NULL && q==NULL) return true;
+5        if(p==NULL || q==NULL || p->val!=q->val) return false;
+6        bool a = func(p->left , q->left);
+7        bool b = func(p->right , q->right);
+8        return a&&b;
+9    }
+10public:
+11    bool isSameTree(TreeNode* p, TreeNode* q) {
+12        return func(p,q);
+13    }
+14};
