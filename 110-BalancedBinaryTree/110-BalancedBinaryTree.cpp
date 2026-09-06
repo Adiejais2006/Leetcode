0230c6cecf78d9f7e67c5bdefa03f0a22c403d05
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 15:33:48 +0530

    File updated at 9/6/2026, 3:33:47 PM

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.cpp
@@ -1,21 +1,21 @@
-// Last updated: 9/6/2026, 3:29:44 PM
+// Last updated: 9/6/2026, 3:33:47 PM
 1class Solution {
-2    int height(TreeNode* root) {
-3        if (root == NULL)
-4            return 0;
-5
-6        return 1 + max(height(root->left), height(root->right));
-7    }
-8    bool balance(TreeNode* root) {
-9        if (root == NULL)
-10            return true;
-11        int lh = height(root->left);
-12        int rh = height(root->right);
-13        if (abs(lh - rh) > 1)
-14            return false;
-15        return balance(root->left) && balance(root->right);
-16    }
-17
-18public:
-19    bool isBalanced(TreeNode* root) { return balance(root); }
+2
+3    int balance(TreeNode* root) {
+4        if (root == NULL)
+5            return 0;
+6        int left = balance(root->left);
+7        int right = balance(root->right);
+8        if (left == -1 || right == -1)
+9            return -1;
+10        if (abs(left - right) > 1)
+11            return -1;
+12        return 1 + max(left, right);
+13    }
+14
+15public:
+16    bool isBalanced(TreeNode* root) {
+17        int a = balance(root);
+18        return a != -1;
+19    }
 20};
