4289909ea97ae682072bcba6172eff78e74efaca
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 12:56:13 +0530

    File updated at 9/6/2026, 12:56:12 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,29 +1,25 @@
-// Last updated: 7/18/2026, 2:06:25 PM
-class Solution {
-public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-
-        vector<unsigned long long> before(m + 1, 0),
-                                   curr(m + 1, 0);
-
-        before[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            curr[0] = 1;
-
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == t[j - 1]) {
-                    curr[j] = before[j - 1] + before[j];
-                } else {
-                    curr[j] = before[j];
-                }
-            }
-
-            before = curr;
-        }
-
-        return before[m];
-    }
-};
+// Last updated: 9/6/2026, 12:56:12 PM
+1class Solution {
+2    int func(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+3        if (j == t.size())
+4            return 1;
+5        if (i == s.size())
+6            return 0;
+7        if (dp[i][j] != -1)
+8            return dp[i][j];
+9        int nottake = func(i + 1, j, s, t, dp);
+10        int take = 0;
+11        if (s[i] == t[j]) {
+12            take = func(i + 1, j + 1, s, t, dp);
+13        }
+14        return dp[i][j] = take + nottake;
+15    }
+16
+17public:
+18    int numDistinct(string s, string t) {
+19        int n = s.size();
+20        int m = t.size();
+21        vector<vector<int>> dp(n, vector<int>(m, -1));
+22        return func(0, 0, s, t, dp);
+23    }
+24};
