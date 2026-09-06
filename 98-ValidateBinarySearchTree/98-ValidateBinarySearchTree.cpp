5c023f3e997f6ba08974d79b6e017f2ed795ab93
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 18:55:09 +0530

    File updated at 9/6/2026, 6:55:08 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
@@ -1,17 +1,18 @@
-// Last updated: 7/18/2026, 2:06:41 PM
-
-class Solution {
-    bool checker(TreeNode* root,  long long low, long long high) {
-        if (root == NULL)
-            return true;
-        if (root->val <= low || root->val >= high)
-            return false;
-        return checker(root->left, low, root->val) &&
-               checker(root->right, root->val, high);
-    }
-
-public:
-    bool isValidBST(TreeNode* root) {
-        return checker(root, LLONG_MIN, LLONG_MAX);
-    }
-};
+// Last updated: 9/6/2026, 6:55:08 PM
+1
+2class Solution {
+3    bool checker(TreeNode* root, long long low, long long high) {
+4        if (root == NULL)
+5            return true;
+6        if (root->val >= high || root -> val <= low)
+7            return false;
+8        bool a = checker(root->left, low, root->val);
+9        bool b = checker(root->right, root->val, high);
+10        return a && b;
+11    }
+12
+13public:
+14    bool isValidBST(TreeNode* root) {
+15        return checker(root, LLONG_MIN, LLONG_MAX);
+16    }
+17};
