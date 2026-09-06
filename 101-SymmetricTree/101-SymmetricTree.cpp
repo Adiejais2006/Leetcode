984f5fe16a2713ecf874b5155b9df293fc479801
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 14:59:18 +0530

    File updated at 9/6/2026, 2:59:17 PM

--- 101-SymmetricTree/101-SymmetricTree.cpp
+++ 101-SymmetricTree/101-SymmetricTree.cpp
@@ -1,16 +1,15 @@
-// Last updated: 7/18/2026, 2:06:37 PM
-
-class Solution {  bool func(TreeNode* leftP, TreeNode* rightP) {
-       if(leftP==NULL || rightP==NULL) return leftP==rightP;
-        if (leftP->val != rightP->val)
-            return false;
-        bool left = func(leftP->left, rightP->right);
-        bool right = func(leftP->right, rightP->left);
-        return left && right;
-    }
-public:
-    bool isSymmetric(TreeNode* root) {
-          if (!root) return true;
-        return func(root->left, root->right);
-    }
-};
+// Last updated: 9/6/2026, 2:59:17 PM
+1class Solution {
+2    bool func(TreeNode* a, TreeNode* b) {
+3        if (a == NULL && b == NULL)
+4            return true;
+5        if (a == NULL || b == NULL || a->val != b->val)
+6            return false;
+7        bool movea = func(a->left, b->right);
+8        bool moveb = func(a->right, b->left);
+9        return movea && moveb;
+10    }
+11
+12public:
+13    bool isSymmetric(TreeNode* root) { return func(root, root); }
+14};
