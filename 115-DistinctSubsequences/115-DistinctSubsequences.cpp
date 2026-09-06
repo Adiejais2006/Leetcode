9d769049cf25c9b2ec876aafa7c96cfe797443b1
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 13:29:31 +0530

    File updated at 9/6/2026, 1:29:31 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/6/2026, 1:26:28 PM
+// Last updated: 9/6/2026, 1:29:31 PM
 1class Solution {
 2
 3public:
@@ -7,21 +7,22 @@
 6        int m = t.size();
 7        if (n < m)
 8            return 0;
-9        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-10        int MOD = (int)1e9 + 7; // to avoid int overflow
-11        for (int i = 0; i <= n; i++) {
-12            dp[i][m] = 1;
-13        }
+9        vector<int> cur(m + 1, 0);
+10        vector<int> prev(m + 1, 0);
+11        int MOD = (int)1e9 + 7;
+12        prev[m] = 1;
+13        cur[m] = 1;
 14        for (int i = n - 1; i >= 0; i--) {
 15            for (int j = m - 1; j >= 0; j--) {
-16                int nottake = dp[i + 1][j];
+16                int nottake = prev[j];
 17                int take = 0;
 18                if (s[i] == t[j]) {
-19                    take = dp[i + 1][j + 1];
+19                    take = prev[j + 1];
 20                }
-21                dp[i][j] = (take + nottake) % MOD;
+21                cur[j] = (take + nottake) % MOD;
 22            }
-23        }
-24        return dp[0][0];
-25    }
-26};
+23            prev = cur;
+24        }
+25        return cur[0];
+26    }
+27};
