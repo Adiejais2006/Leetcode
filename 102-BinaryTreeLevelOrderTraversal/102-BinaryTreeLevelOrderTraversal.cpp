770099d4cca23b69ad9c241e825ce1032e12c476
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 15:05:32 +0530

    File updated at 9/6/2026, 3:05:31 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -1,36 +1,27 @@
-// Last updated: 7/18/2026, 2:06:36 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(!root) return {};
-        vector<vector<int>> ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> temp;
-            while (size > 0) {
-                TreeNode* node = q.front();
-                q.pop();
-                temp.push_back(node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-                size--;
-            }
-            ans.push_back(temp);
-        }
-
-        return ans;
-    }
-};
+// Last updated: 9/6/2026, 3:05:31 PM
+1
+2class Solution {
+3public:
+4    vector<vector<int>> levelOrder(TreeNode* root) {
+5        if (root == NULL)
+6            return {};
+7        vector<vector<int>> ans;
+8        queue<TreeNode*> q;
+9        q.push(root);
+10        while (!q.empty()) {
+11            auto size = q.size();
+12            vector<int> temp;
+13            while (size--) {
+14                auto a = q.front();
+15                q.pop();
+16                temp.push_back(a->val);
+17                if (a->left != NULL)
+18                    q.push(a->left);
+19                if (a->right != NULL)
+20                    q.push(a->right);
+21            }
+22            ans.push_back(temp);
+23        }
+24        return ans;
+25    }
+26};
