6b28bb31bb8b4edad01b713036f7dba8b468ca19
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 13:26:28 +0530

    File updated at 9/6/2026, 1:26:28 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,25 +1,27 @@
-// Last updated: 9/6/2026, 12:56:12 PM
+// Last updated: 9/6/2026, 1:26:28 PM
 1class Solution {
-2    int func(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-3        if (j == t.size())
-4            return 1;
-5        if (i == s.size())
-6            return 0;
-7        if (dp[i][j] != -1)
-8            return dp[i][j];
-9        int nottake = func(i + 1, j, s, t, dp);
-10        int take = 0;
-11        if (s[i] == t[j]) {
-12            take = func(i + 1, j + 1, s, t, dp);
+2
+3public:
+4    int numDistinct(string s, string t) {
+5        int n = s.size();
+6        int m = t.size();
+7        if (n < m)
+8            return 0;
+9        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+10        int MOD = (int)1e9 + 7; // to avoid int overflow
+11        for (int i = 0; i <= n; i++) {
+12            dp[i][m] = 1;
 13        }
-14        return dp[i][j] = take + nottake;
-15    }
-16
-17public:
-18    int numDistinct(string s, string t) {
-19        int n = s.size();
-20        int m = t.size();
-21        vector<vector<int>> dp(n, vector<int>(m, -1));
-22        return func(0, 0, s, t, dp);
-23    }
-24};
+14        for (int i = n - 1; i >= 0; i--) {
+15            for (int j = m - 1; j >= 0; j--) {
+16                int nottake = dp[i + 1][j];
+17                int take = 0;
+18                if (s[i] == t[j]) {
+19                    take = dp[i + 1][j + 1];
+20                }
+21                dp[i][j] = (take + nottake) % MOD;
+22            }
+23        }
+24        return dp[0][0];
+25    }
+26};
