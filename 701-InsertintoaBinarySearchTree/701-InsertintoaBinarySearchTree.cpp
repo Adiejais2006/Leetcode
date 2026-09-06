c27d6ae1b9c35e49ee977cffc53d27d2461cd0dd
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-06 22:34:23 +0530

    File updated at 9/6/2026, 10:34:22 PM

--- 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.cpp
@@ -1,22 +1,21 @@
-// Last updated: 9/6/2026, 9:13:37 PM
+// Last updated: 9/6/2026, 10:34:22 PM
 1
 2class Solution {
 3
 4    TreeNode* insert(TreeNode* root, int val) {
-5        if (root == NULL) {
-6            return new TreeNode(val);
-7        }
-8        if (root->val > val) {
-9            root->left = insert(root->left, val);
-10        }
-11        if (root->val < val) {
-12            root->right = insert(root->right, val);
-13        }
-14        return root;
-15    }
-16
-17public:
-18    TreeNode* insertIntoBST(TreeNode* root, int val) {
-19        return insert(root, val);
-20    }
-21};
+5        if (root == NULL)
+6           return new TreeNode(val);
+7        if (root->val > val) {
+8            root->left = insert(root->left, val);
+9        }
+10        if (root->val < val) {
+11            root->right = insert(root->right, val);
+12        }
+13        return root;
+14    }
+15
+16public:
+17    TreeNode* insertIntoBST(TreeNode* root, int val) {
+18        return insert(root, val);
+19    }
+20};
