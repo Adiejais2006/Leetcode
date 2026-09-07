552c7f1f088466e2fe4bae30988b399329bcf594
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-07 18:33:50 +0530

    File updated at 9/7/2026, 6:33:49 PM

--- 114-FlattenBinaryTreetoLinkedList/114-FlattenBinaryTreetoLinkedList.cpp
+++ 114-FlattenBinaryTreetoLinkedList/114-FlattenBinaryTreetoLinkedList.cpp
@@ -1,17 +1,24 @@
-// Last updated: 9/7/2026, 6:21:03 PM
-1class Solution {
-2    void change(TreeNode* root, TreeNode* &prev){
-3        if(root==NULL)return;
-4        change(root->right , prev);
-5        change(root->left , prev);
-6        root->right = prev;
-7        root->left = NULL;
-8        prev = root;
-9        
-10    }
-11public:
-12    void flatten(TreeNode* root) {
-13        TreeNode* prev = NULL;
-14        change(root,prev);
-15    }
-16};
+// Last updated: 9/7/2026, 6:33:49 PM
+1
+2class Solution {
+3public:
+4    void flatten(TreeNode* root) {
+5        if (!root)
+6            return;
+7        stack<TreeNode*> st;
+8        st.push(root);
+9        while (!st.empty()) {
+10            auto node = st.top();
+11            st.pop();
+12            if (node->right != NULL)
+13                st.push(node->right);
+14            if (node->left != NULL)
+15                st.push(node->left);
+16            if (!st.empty())
+17                node->right = st.top();
+18            else
+19                node->right = NULL;
+20            node->left = NULL;
+21        }
+22    }
+23};
