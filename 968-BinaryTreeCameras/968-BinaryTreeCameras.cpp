bff389bb788ee3b50f79dadf9a13ee825744af8e
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-08 23:43:03 +0530

    File updated at 9/8/2026, 11:43:03 PM

--- 968-BinaryTreeCameras/968-BinaryTreeCameras.cpp
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.cpp
@@ -1,24 +1,19 @@
-// Last updated: 9/8/2026, 11:34:02 PM
+// Last updated: 9/8/2026, 11:43:03 PM
 1class Solution {
-2    int cameras = 0;
-3    int dfs(TreeNode* root) {
-4        if (root == NULL)
-5            return 2; 
-6        int left = dfs(root->left);
-7        int right = dfs(root->right);
-8        if (left == 0 || right == 0) {
-9            cameras++;
-10            return 1;
-11        }
-12        if (left == 1 || right == 1) {
-13            return 2; 
-14        }
-15        return 0;
-16    }
-17public:
-18    int minCameraCover(TreeNode* root) {
-19        if (dfs(root) == 0)
-20            cameras++;
-21        return cameras;
-22    }
-23};
+2public:
+3    vector<int> dfs(TreeNode* node){
+4        if( !node ) return {99999, 99999, 0} ;
+5        vector<int> l = dfs(node->left) ;
+6        vector<int> r = dfs(node->right) ;
+7        int zero = l[2] + r[2] ;
+8        int one = 1 + min({l[0], l[1], l[2]}) + min({r[0], r[1], r[2]}) ;
+9        int two_A = l[1] + min(r[1], r[2]) ;
+10        int two_B = r[1] + min(l[1], l[2]) ;
+11        int two_res = min(two_A, two_B) ;
+12        return {zero, one, two_res} ;
+13    }
+14    int minCameraCover(TreeNode* root) {
+15        vector<int> ans = dfs(root) ;
+16        return min(ans[1], ans[2]) ;
+17    }
+18};
