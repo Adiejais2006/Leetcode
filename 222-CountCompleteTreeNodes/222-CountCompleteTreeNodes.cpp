488b53961987cccee5c81924de686edb3b8add1b
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-08 18:51:12 +0530

    File updated at 9/8/2026, 6:51:12 PM

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.cpp
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.cpp
@@ -1,24 +1,27 @@
-// Last updated: 7/18/2026, 2:05:24 PM
-
-class Solution {
-public:
-    int countNodes(TreeNode* root) {
-        if(root==NULL) return 0;
-         long long  ans = 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-        while (!q.empty()) {
-            TreeNode* node = q.front().first;
-            long long count = q.front().second;
-            q.pop();
-            ans = max(ans, count);
-            if (node->left != NULL) {
-                q.push({node->left, 2*count+1});
-            }
-            if (node->right != NULL) {
-                q.push({node->right, 2*count+2});
-            }
-        }
-        return (int)ans+1;
-    }
-};
+// Last updated: 9/8/2026, 6:51:12 PM
+1class Solution {
+2    int leftheight(TreeNode* root) {
+3        if (root == NULL)
+4            return 0;
+5        return 1 + leftheight(root->left);
+6    }
+7    int rightheight(TreeNode* root) {
+8        if (root == NULL)
+9            return 0;
+10        return 1 + rightheight(root->right);
+11    }
+12    int nodes(TreeNode* root) {
+13        if (root == NULL)
+14            return 0;
+15
+16        int lh = leftheight(root);
+17        int rh = rightheight(root);
+18        if (lh == rh) {
+19            return (1 << lh) - 1;
+20        }
+21        return 1 + nodes(root->left) + nodes(root->right);
+22    }
+23
+24public:
+25    int countNodes(TreeNode* root) { return nodes(root); }
+26};
