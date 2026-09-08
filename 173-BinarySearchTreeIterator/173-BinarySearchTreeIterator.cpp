7d75b656e1e7b79e14dfe20b527afd2578bdae92
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-08 18:42:16 +0530

    File updated at 9/8/2026, 6:42:16 PM

--- 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.cpp
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.cpp
@@ -1,30 +1,29 @@
-// Last updated: 7/18/2026, 2:05:47 PM
-
-class BSTIterator {
-    stack<TreeNode*> st;
-    TreeNode* node;
-    void left(TreeNode* root, stack<TreeNode*>& st) {
-        if (root == NULL)
-            return;
-        st.push(root);
-        left(root->left, st);
-    }
-
-public:
-    BSTIterator(TreeNode* root) {
-        node = root;
-        left(node, st);
-    }
-
-    int next() {
-        TreeNode* nodee = st.top();
-        st.pop();
-        int ele = nodee->val;
-
-        if (nodee->right != NULL)
-            left(nodee->right, st);
-        return ele;
-    }
-
-    bool hasNext() { return !st.empty(); }
-};
+// Last updated: 9/8/2026, 6:42:16 PM
+1
+2class BSTIterator {
+3    stack<TreeNode*> st;
+4    void left(TreeNode* root, stack<TreeNode*>& st) {
+5        if (root == NULL)
+6            return;
+7        st.push(root);
+8        left(root->left, st);
+9    }
+10
+11public:
+12    BSTIterator(TreeNode* root) {
+13        stack<TreeNode*> a;
+14        left(root, a);
+15        st = a;
+16    }
+17
+18    int next() {
+19        auto node = st.top();
+20        st.pop();
+21        if (node->right)
+22            left(node->right, st);
+23        return node->val;
+24    }
+25
+26    bool hasNext() { return !st.empty(); }
+27};
+28
