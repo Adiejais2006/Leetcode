71e3a1bc5b84f7feb584f3290631453533d8d35b
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-11 12:41:19 +0530

    File updated at 9/11/2026, 12:41:18 PM

--- 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.cpp
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.cpp
@@ -1,23 +1,24 @@
-// Last updated: 9/11/2026, 12:37:06 PM
+// Last updated: 9/11/2026, 12:41:18 PM
 1class Solution {
 2public:
 3    int totalNumbers(vector<int>& digits) {
-4        unordered_set<int> seen;
-5        int n = digits.size();
-6        for(int h = 0; h < n; h++){
-7            if (digits[h] == 0) continue;
-8            for(int t = 0; t < n; t++){
-9                if (t == h) continue;
-10                for(int u = 0; u < n; u++){
-11                    if(u == h || u == t) continue;
-12                    if(digits[u] % 2 != 0) continue;
-13
-14                    int num = digits[h]*100+digits[t]*10+digits[u];
-15                    seen.insert(num);
+4        int freq[10] = {0};
+5        for (int d : digits) freq[d]++;
+6        int count = 0;
+7
+8        for(int h = 1; h <= 9; h++){
+9            if (freq[h] == 0) continue;
+10            freq[h]--;
+11            for(int t = 0; t <= 9; t++){
+12                if (freq[t] == 0) continue;
+13                freq[t]--;
+14                for(int u = 0; u <= 8; u += 2){
+15                    if (freq[u] > 0) count++;
 16                }
-17            }
-18        }
-19
-20        return seen.size();
-21    }
-22};
+17                freq[t]++;
+18            }
+19            freq[h]++;
+20        }
+21        return count;
+22    }
+23};
