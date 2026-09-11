0ebae55f8420d2b99734f7bdadead70b3aa31805
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-11 15:37:43 +0530

    File updated at 9/11/2026, 3:37:43 PM

--- 993-CousinsinBinaryTree/993-CousinsinBinaryTree.cpp
+++ 993-CousinsinBinaryTree/993-CousinsinBinaryTree.cpp
@@ -1,25 +1,30 @@
-// Last updated: 9/11/2026, 3:02:01 PM
+// Last updated: 9/11/2026, 3:37:43 PM
 1class Solution {
-2public:int xdepth, ydepth, xparent, yparent;
-3void depth(TreeNode* root, int x, int y, int dep, int par_val){
-4    if(root==NULL) return;
-5    if(root->val==x){
-6        xdepth=dep;
-7        xparent=par_val;
-8        return;
-9    }
-10    if(root->val==y){
-11        ydepth=dep;
-12        yparent=par_val;
-13        return;
-14    }
-15    depth(root->left, x, y, dep+1, root->val);
-16    depth(root->right, x, y, dep+1, root->val);
-17}
-18bool isCousins(TreeNode* root, int x, int y) {
-19    if(root->val==x || root->val==y) return false;
-20    depth(root, x, y, 0, 0);
-21    if(xdepth==ydepth && xparent!=yparent) return true;
-22    return false;
-23}
-24};
+2public:
+3    bool isCousins(TreeNode* root, int x, int y) {
+4        queue<pair<TreeNode*, TreeNode*>> q;
+5        q.push({root, nullptr});
+6        while (!q.empty()) {
+7            int size = q.size();
+8            TreeNode* parentX = nullptr;
+9            TreeNode* parentY = nullptr;
+10            while (size--) {
+11                auto [node, parent] = q.front();
+12                q.pop();
+13                if (node->val == x)
+14                    parentX = parent;
+15                if (node->val == y)
+16                    parentY = parent;
+17                if (node->left)
+18                    q.push({node->left, node});
+19                if (node->right)
+20                    q.push({node->right, node});
+21            }
+22            if (parentX && parentY)
+23                return parentX != parentY;
+24            if (parentX || parentY)
+25                return false;
+26        }
+27        return false;
+28    }
+29};
