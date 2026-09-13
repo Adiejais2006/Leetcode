683a48631ccf44c5a9fe20130e4b4e22d3ca4625
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-13 15:04:50 +0530

    File updated at 9/13/2026, 3:04:49 PM

--- 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.cpp
+++ 442-FindAllDuplicatesinanArray/442-FindAllDuplicatesinanArray.cpp
@@ -1,17 +1,14 @@
-// Last updated: 9/13/2026, 3:03:18 PM
+// Last updated: 9/13/2026, 3:04:49 PM
 1class Solution {
 2public:
 3    vector<int> findDuplicates(vector<int>& nums) {
-4        unordered_map<int, int> mpp;
-5        for (auto it : nums) {
-6            mpp[it]++;
-7        }
-8        vector<int>ans;
-9        for(auto it :mpp){
-10            if(it.second==2){
-11                ans.push_back(it.first);
-12            }
-13        }
-14        return ans;
-15    }
-16};
+4        if(nums.empty())return {};
+5        vector<int>ans;
+6        for(int i=0;i<nums.size();i++){
+7            if(nums[abs(nums[i])-1]<0)
+8                ans.push_back(abs(nums[i]));
+9            nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+10        }
+11        return ans;
+12    }
+13};
