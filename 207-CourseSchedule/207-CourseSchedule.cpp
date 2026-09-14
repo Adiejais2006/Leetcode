88af07ff6692686363220dbfd668184acd8774bc
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-14 16:13:44 +0530

    File updated at 9/14/2026, 4:13:42 PM

--- 207-CourseSchedule/207-CourseSchedule.cpp
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -1,30 +1,37 @@
-// Last updated: 7/18/2026, 2:05:37 PM
-class Solution {
-public:
-    bool canFinish(int N, vector<vector<int>>& arr) {
-        vector<int> adj[N];
-        vector<int> indegree(N, 0);
-        for (int i = 0; i < arr.size(); i++) {
-            int u = arr[i][1];
-            int v = arr[i][0];
-            adj[u].push_back(v);
-            indegree[v]++;
-        }
-        queue<int> q;
-        for (int i = 0; i < N; i++) {
-            if (indegree[i] == 0) q.push(i);
-        }
-        vector<int> temp;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            temp.push_back(node);
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0) q.push(it);
-            }
-        }
-
-        return temp.size() == N;
-    }
-};
+// Last updated: 9/14/2026, 4:13:42 PM
+1class Solution {
+2    bool cycle(int node, unordered_set<int>& st, vector<int> adj[],
+3               vector<int>& vis) {
+4        vis[node] = 1;
+5        st.insert(node);
+6        for (auto it : adj[node]) {
+7            if (!vis[it]) {
+8                vis[it] = 1;
+9                st.insert(it);
+10                if (cycle(it, st, adj, vis))
+11                    return true;
+12
+13            } else if (st.count(it))
+14                return true;
+15        }
+16        st.erase(node);
+17        return false;
+18    }
+19
+20public:
+21    bool canFinish(int numCourses, vector<vector<int>> prerequisites) {
+22        vector<int> adj[numCourses];
+23        vector<int> vis(numCourses, 0);
+24        unordered_set<int> st;
+25        for (auto it : prerequisites) {
+26            int u = it[0];
+27            int v = it[1];
+28            adj[u].push_back(v);
+29        }
+30        for (int i = 0; i < numCourses; i++) {
+31            if (!vis[i] && cycle(i, st, adj, vis))
+32                return false;
+33        }
+34        return true;
+35    }
+36};
