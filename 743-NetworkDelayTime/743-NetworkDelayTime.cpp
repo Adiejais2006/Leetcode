bf497750bbddfe756c4c7d279954daff382feca7
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-21 19:02:35 +0530

    File updated at 9/21/2026, 7:02:34 PM

--- 743-NetworkDelayTime/743-NetworkDelayTime.cpp
+++ 743-NetworkDelayTime/743-NetworkDelayTime.cpp
@@ -1,40 +1,39 @@
-// Last updated: 9/15/2026, 3:54:14 PM
+// Last updated: 9/21/2026, 7:02:34 PM
 1class Solution {
 2public:
 3    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-4        // making adjacency list
-5        vector<pair<int, int>> adj[n + 1];
-6        for (auto it : times) {
-7            int u = it[0];
-8            int v = it[1];
-9            int wt = it[2];
-10            adj[u].push_back({v, wt});
-11        }
-12        vector<int> dist(n + 1, 1e9);
+4        vector<pair<int, int>> adj[n + 1];
+5        for (auto it : times) {
+6            int u = it[0];
+7            int v = it[1];
+8            int wt = it[2];
+9            adj[u].push_back({v, wt});
+10        }
+11        vector<int> dist(n + 1, 1e9);
+12        dist[k] = 0;
 13        priority_queue<pair<int, int>, vector<pair<int, int>>,
 14                       greater<pair<int, int>>>
-15            q;
-16        q.push({0, k});
-17        dist[k] = 0;
-18        int ans = 0;
-19        while (!q.empty()) {
-20            auto [distance, node] = q.top();
-21            q.pop();
-22            for (auto it : adj[node]) {
-23                int adjNode = it.first;
-24                int adjDist = it.second;
-25                int newDist = distance + adjDist;
-26                if (dist[adjNode] > newDist) {
-27                    dist[adjNode] = newDist;
-28                    q.push({newDist, adjNode});
-29                }
-30            }
-31        }
-32        for (int i = 1; i <= n; i++) {
-33            if (dist[i] == 1e9)
-34                return -1;
-35            ans = max(ans, dist[i]);
-36        }
-37        return ans;
-38    }
-39};
+15            pq;
+16        pq.push({0, k});
+17        while (!pq.empty()) {
+18            auto [currDist, node] = pq.top();
+19            pq.pop();
+20            for (auto it : adj[node]) {
+21                int adjNode = it.first;
+22                int adjWt = it.second;
+23                int totalDist = currDist + adjWt;
+24                if (totalDist < dist[adjNode]) {
+25                    dist[adjNode] = totalDist;
+26                    pq.push({totalDist, adjNode});
+27                }
+28            }
+29        }
+30        int ans = -1;
+31        for (int i = 1; i <= n; i++) {
+32            if (dist[i] == 1e9)
+33                return -1;
+34            ans = max(ans, dist[i]);
+35        }
+36        return ans;
+37    }
+38};
