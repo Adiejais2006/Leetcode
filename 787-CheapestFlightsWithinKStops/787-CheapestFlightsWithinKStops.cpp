46366b662df80418dfbbd6d35c588a7e5c83fbee
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-22 21:32:42 +0530

    File updated at 9/22/2026, 9:32:41 PM

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.cpp
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.cpp
@@ -1,43 +1,22 @@
-// Last updated: 9/15/2026, 6:36:06 PM
+// Last updated: 9/22/2026, 9:32:41 PM
 1class Solution {
 2public:
 3    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
 4                          int k) {
-5        vector<pair<int, int>> adj[n];
-6        vector<int> dist(n, 1e9);
-7        dist[src] = 0 ; 
-8        for (auto it : flights) {
-9            int u = it[0];
-10            int v = it[1];
-11            int wt = it[2];
-12            adj[u].push_back({v, wt});
-13        }
-14        priority_queue<pair<int, pair<int, int>>,
-15                       vector<pair<int, pair<int, int>>>,
-16                       greater<pair<int, pair<int, int>>>>
-17            q;
-18        q.push({0, {0, src}}); // stops dist and nodes
-19        while (!q.empty()) {
-20            auto i = q.top();
-21            q.pop();
-22            int stops = i.first;
-23            int d = i.second.first;
-24            int node = i.second.second;
-25            for (auto it : adj[node]) {
-26                int adjNode = it.first;
-27                int adjDist = it.second;
-28                int newDist = d + adjDist;
-29                if (stops > k)
-30                    continue;
-31                if (dist[adjNode] > newDist) {
-32                    dist[adjNode] = newDist;
-33                    q.push({stops+1, {newDist, adjNode}});
-34                }
-35            }
-36        }
-37        if (dist[dst] == 1e9)
-38            return -1;
-39        return dist[dst];
-40    }
-41};
-42
+5        vector<int> dist(n, 1e9);
+6        dist[src] = 0;
+7        for (int i = 0; i < k + 1; i++) {
+8            vector<int> temp = dist;
+9            for (auto it : flights) {
+10                int u = it[0];
+11                int v = it[1];
+12                int wt = it[2];
+13                if (temp[u] != 1e9 && dist[u] + wt < temp[v]) {
+14                    temp[v] = dist[u] + wt;
+15                }
+16            }   
+17            dist = temp;
+18        }
+19        return dist[dst] == 1e9 ? -1 : dist[dst];
+20    }
+21};
