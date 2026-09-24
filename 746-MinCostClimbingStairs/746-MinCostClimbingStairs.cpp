b89f929fa50d674ac04b9e9eacc633eb77e23ebe
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 13:16:34 +0530

    File updated at 9/24/2026, 1:16:34 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
@@ -1,21 +1,17 @@
-// Last updated: 9/24/2026, 1:10:36 PM
+// Last updated: 9/24/2026, 1:16:34 PM
 1class Solution {
-2    int func(int ind, vector<int>& cost,vector<int>& dp) {
-3        if (ind < 0)
-4            return 0;
-5        if (ind == 0 || ind == 1) {
-6            return cost[ind];
-7        }
-8        if(dp[ind]!=-1) return dp[ind];
-9        int step1 = cost[ind] + func(ind - 1, cost,dp);
-10        int step2 = cost[ind] + func(ind - 2, cost,dp);
-11        return dp[ind] = min(step1, step2);
-12    }
-13
-14public:
-15    int minCostClimbingStairs(vector<int>& cost) {
-16        int n = cost.size();
-17        vector<int> dp(n+1,-1);
-18        return min(func(n - 2, cost,dp), func(n - 1, cost,dp));
-19    }
-20};
+2
+3public:
+4    int minCostClimbingStairs(vector<int>& cost) {
+5        int n = cost.size();
+6        vector<int> dp(n + 1, -1);
+7        dp[0] = cost[0];
+8        dp[1] = cost[1];
+9        for (int ind = 2; ind < n; ind++) {
+10            int step1 = cost[ind] + dp[ind - 1];
+11            int step2 = cost[ind] + dp[ind - 2];
+12            dp[ind] = min(step1, step2);
+13        }
+14        return min(dp[n - 2], dp[n - 1]);
+15    }
+16};
