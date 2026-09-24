d1c001a2c934550839e860c37854603761048afc
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 12:15:44 +0530

    File updated at 9/24/2026, 12:15:43 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,15 +1,20 @@
-// Last updated: 9/24/2026, 12:15:20 PM
-1class Solution { int func(int  n , vector<int>&dp){
-2        if(n==0) return 1;
-3        if(n<0) return 0 ;
-4        if(dp[n]!=-1) return dp[n];
-5        int left = func(n-1,dp);
-6        int right = func(n-2,dp);
-7        return dp[n] = left + right;
-8    }
-9public:
-10    int climbStairs(int n) {
-11        vector<int> dp(n+1,-1);
-12        return func(n ,dp);
-13    }
-14};
+// Last updated: 9/24/2026, 12:15:43 PM
+1class Solution {
+2    int func(int n, vector<int>& dp) {
+3        if (n < 0)
+4            return 0;
+5        if (n == 0) {
+6            return 1;
+7        }
+8        if(dp[n]!=-1)return dp[n];
+9        int way1 = func(n - 1, dp);
+10        int way2 = func(n - 2, dp);
+11        return dp[n] =way1 + way2;
+12    }
+13
+14public:
+15    int climbStairs(int n) {
+16        vector<int> dp(n+1, -1);
+17        return func(n, dp);
+18    }
+19};
