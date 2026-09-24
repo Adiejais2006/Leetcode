f0e9f98f849c23c27da61a5db4345635caccd42f
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 13:23:57 +0530

    File updated at 9/24/2026, 1:23:57 PM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,17 +1,17 @@
-// Last updated: 7/18/2026, 2:05:43 PM
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        int prev = nums[0];
-        int prev2= 0;
-        for (int i = 1; i < n; i++) {
-            int take = nums[i] + ((i > 1) ? prev2 : 0);
-            int nottake = prev;
-            int cur =  max(take, nottake);
-            prev2=prev;
-            prev =cur;
-        }
-        return prev;
-    }
-};
+// Last updated: 9/24/2026, 1:23:57 PM
+1class Solution {
+2public:
+3    int rob(vector<int>& nums) {
+4         int n = nums.size();
+5        vector<int> dp(n + 1, -1);
+6        dp[0] = nums[0];
+7        for (int i = 1; i < n; i++) {
+8            int left = nums[i] ;
+9          if(i>1) left = nums[i] + dp[i-2];
+10          int right = dp[i-1];
+11          dp[i] = max(left,right);
+12        }
+13
+14        return dp[n - 1];
+15    }
+16};
