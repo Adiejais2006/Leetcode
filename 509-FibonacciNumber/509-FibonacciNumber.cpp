fe5d9a552ccb332e4b7bee0206b48369826eb71d
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 12:07:53 +0530

    File updated at 9/24/2026, 12:07:52 PM

--- 509-FibonacciNumber/509-FibonacciNumber.cpp
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -1,15 +1,17 @@
-// Last updated: 9/24/2026, 12:06:40 PM
+// Last updated: 9/24/2026, 12:07:52 PM
 1class Solution {
 2
 3public:
 4    int fib(int n) {
 5        if(n==0)return 0;
 6        vector<int> dp(n + 1, 0);
-7        dp[0] = 0;
-8        dp[1] = 1;
+7        int a=0;
+8        int b=1;
 9        for (int i = 2; i <= n; i++) {
-10            dp[i] = dp[i - 1]+ dp[i - 2];
-11        }
-12        return dp[n];
-13    }
-14};
+10           int curr = a+ b;
+11           a=b;
+12           b=curr;
+13        }
+14        return b;
+15    }
+16};
