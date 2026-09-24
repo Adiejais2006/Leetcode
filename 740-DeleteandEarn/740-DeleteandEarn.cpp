0b9096156d21f919d4cb5818fbedebff03f0e8ab
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 20:56:13 +0530

    File updated at 9/24/2026, 8:56:13 PM

--- 740-DeleteandEarn/740-DeleteandEarn.cpp
+++ 740-DeleteandEarn/740-DeleteandEarn.cpp
@@ -1,23 +1,24 @@
-// Last updated: 9/24/2026, 8:51:26 PM
+// Last updated: 9/24/2026, 8:56:13 PM
 1class Solution {
-2
-3public:
-4    int deleteAndEarn(vector<int>& nums) {
-5        int n = nums.size();
-6        int mx = *max_element(nums.begin(), nums.end());
-7        vector<int> gain(mx + 1, 0);
-8        for (int x : nums) {
-9            gain[x] += x;
-10        }
-11        vector<int> dp(mx + 1, 0);
-12        dp[0] = gain[0];
-13        if(mx>=1)
-14        dp[1] = gain[1];
-15        for (int i = 2; i <= mx; i++) {
-16            int notpick = dp[i - 1];
-17            int pick = gain[i] + dp[i - 2];
-18            dp[i] = max(pick, notpick);
-19        }
-20        return dp[mx];
-21    }
-22};
+2public:
+3    int deleteAndEarn(vector<int>& nums) {
+4        int mx = *max_element(nums.begin(), nums.end());
+5        vector<int> gain(mx + 1, 0);
+6        for (int x : nums) {
+7            gain[x] += x;
+8        }
+9
+10        if (mx == 0)
+11            return 0;
+12        int prev2 = gain[0]; 
+13        int prev = gain[1];   
+14        for (int i = 2; i <= mx; i++) {
+15            int notpick = prev;
+16            int pick = gain[i] + prev2;
+17            int curr = max(pick, notpick);
+18            prev2 = prev;
+19            prev = curr;
+20        }
+21        return prev;
+22    }
+23};
