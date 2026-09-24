ebec28a2aae0da71989c3f9e8026d0183c6ab160
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 12:22:16 +0530

    File updated at 9/24/2026, 12:22:15 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,15 +1,14 @@
-// Last updated: 9/24/2026, 12:20:20 PM
+// Last updated: 9/24/2026, 12:22:15 PM
 1class Solution {
 2public:
 3    int climbStairs(int n) {
-4        vector<int> dp(n + 1, 0);
-5        dp[0] = 1;
-6        dp[1] = 1 ;
-7        for (int i = 2; i <= n; i++) {
-8            int way1 =dp[i - 1];
-9            int way2 = dp[i - 2];
-10           dp[i] = way1 + way2;
-11        }
-12        return dp[n];
-13    }
-14};
+4        int prev1 = 1;
+5        int prev2 = 1;
+6        for (int i = 2; i <= n; i++) {
+7            int curr = prev1 + prev2;
+8            prev1 = prev2;
+9            prev2 = curr;
+10        }
+11        return prev2;
+12    }
+13};
