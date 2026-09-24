54f44eb9830152673b283ff32970aa6e05ed7c09
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-24 12:04:01 +0530

    File updated at 9/24/2026, 12:04:00 PM

--- 509-FibonacciNumber/509-FibonacciNumber.cpp
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -1,12 +1,18 @@
-// Last updated: 9/24/2026, 12:02:23 PM
+// Last updated: 9/24/2026, 12:04:00 PM
 1class Solution {
-2    int func(int n){
-3        if(n==0)return 0;
-4        if(n==1) return 1;
-5        return func(n-1) + func(n-2);
-6    }
-7public:
-8    int fib(int n) {
-9        return func(n);
+2    int func(int n, vector<int>& dp) {
+3        if (n == 0)
+4            return 0;
+5        if (n == 1)
+6            return 1;
+7        if (dp[n] != -1)
+8            return dp[n];
+9        return dp[n] = func(n - 1, dp) + func(n - 2, dp);
 10    }
-11};
+11
+12public:
+13    int fib(int n) {
+14        vector<int> dp(n + 1, -1);
+15        return func(n, dp);
+16    }
+17};
