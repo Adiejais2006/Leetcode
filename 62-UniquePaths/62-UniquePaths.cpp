8d448e341a0878eb1486ca37f238db2053b2364b
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-25 17:01:22 +0530

    File updated at 9/25/2026, 5:01:23 PM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,22 +1,20 @@
-// Last updated: 7/18/2026, 2:07:07 PM
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<int> prev(n, 0);
-        prev[0] = 1;
-        for (int i = 0; i < m; i++) {
-            vector<int> temp(n,0);
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    temp[j] =1;
-                    continue;
-                }
-                int up = prev[j];
-               int left = (j > 0) ? temp[j - 1] : 0;
-               temp[j] = up + left;
-            }
-            prev = temp;
-        }
-        return prev[n - 1];
-    }
-};
+// Last updated: 9/25/2026, 5:01:23 PM
+1class Solution {
+2    int func(int i, int j, vector<vector<int>>& dp) {
+3        if (i == 0 && j == 0)
+4            return 1;
+5        if (i < 0 || j < 0)
+6            return 0;
+7        if (dp[i][j] != -1)
+8            return dp[i][j];
+9        int up = func(i - 1, j, dp);
+10        int left = func(i, j - 1, dp);
+11        return dp[i][j] = up + left;
+12    }
+13
+14public:
+15    int uniquePaths(int m, int n) {
+16        vector<vector<int>> dp(m, vector<int>(n, -1));
+17        return func(m - 1, n - 1, dp);
+18    }
+19};
