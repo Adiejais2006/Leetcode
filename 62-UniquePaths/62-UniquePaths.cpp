5b5b99fe71c90b939eadec84f47a144656dca80f
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-25 17:08:38 +0530

    File updated at 9/25/2026, 5:08:38 PM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,16 +1,18 @@
-// Last updated: 9/25/2026, 5:06:14 PM
+// Last updated: 9/25/2026, 5:08:38 PM
 1class Solution {
-2public:
-3    int uniquePaths(int m, int n) {
-4        vector<vector<int>> dp(m, vector<int>(n, 1));
-5
-6
-7        for(int i = 1 ; i< m ; i++){
-8            for(int j =  1 ; j < n ; j++){
-9
-10                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-11            }
-12        }
-13        return dp[m-1][n-1];
-14    }
-15};
+2
+3public:
+4    int uniquePaths(int m, int n) {
+5        vector<int> prev(n, 1);
+6        vector<int> curr(n, 1);
+7        for (int i = 1; i < m; i++) {
+8            for (int j = 1; j < n; j++) {
+9                int up = prev[j];
+10                int left = curr[j - 1];
+11                curr[j] = up + left;
+12                prev = curr;
+13            }
+14        }
+15        return prev[n - 1];
+16    }
+17};
