b85a0d228315b934b63072107f5ae919dde10a96
Aditya Jaiswal <aditya_24uics202@iiitagartala.ac.in>
2026-09-25 17:32:04 +0530

    File updated at 9/25/2026, 5:32:04 PM

--- 63-UniquePathsII/63-UniquePathsII.cpp
+++ 63-UniquePathsII/63-UniquePathsII.cpp
@@ -1,28 +1,31 @@
-// Last updated: 9/25/2026, 5:29:49 PM
+// Last updated: 9/25/2026, 5:32:04 PM
 1class Solution {
-2public:
-3    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-4        int m = obstacleGrid.size();
-5        int n = obstacleGrid[0].size();
-6        if (obstacleGrid[0][0] == 1)
-7            return 0;
-8        vector<vector<int>> dp(m, vector<int>(n, 0));
-9        dp[0][0] = 1;
-10        for (int i = 1; i < m; i++) {
-11            if (obstacleGrid[i][0] == 0)
-12                dp[i][0] = dp[i - 1][0];
+2
+3public:
+4    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+5        int m = obstacleGrid.size();
+6        int n = obstacleGrid[0].size();
+7        if (obstacleGrid[0][0] == 1)
+8            return 0;
+9        vector<vector<int>> dp(m, vector<int>(n, 0));
+10        for (int i = 0; i < m; i++) {
+11            if (obstacleGrid[i][0] == 1) break;
+12                dp[i][0] = 1;
 13        }
-14        for (int j = 1; j < n; j++) {
-15            if (obstacleGrid[0][j] == 0)
-16                dp[0][j] = dp[0][j - 1];
+14        for (int i = 0; i < n; i++) {
+15             if (obstacleGrid[0][i] == 1) break;
+16                dp[0][i] = 1;
 17        }
 18        for (int i = 1; i < m; i++) {
 19            for (int j = 1; j < n; j++) {
-20                if (obstacleGrid[i][j] == 1)
-21                    continue;
-22                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-23            }
-24        }
-25        return dp[m - 1][n - 1];
-26    }
-27};
+20                if(obstacleGrid[i][j]!=1){
+21
+22                int up = dp[i - 1][j];
+23                int left = dp[i][j - 1];
+24                dp[i][j] = up + left;
+25                }
+26            }
+27        }
+28        return dp[m - 1][n - 1];
+29    }
+30};
